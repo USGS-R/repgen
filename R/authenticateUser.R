1ee6fc4a13a48a8c8919fe361fd02b9fb72be1cb
Jordan S Read <jread@usgs.gov>
2015-03-12 15:07:26 -0500

    breaking out functionality. overloading extremes

---
+++ R/authenticateUser.R
@@ -0,0 +1,32 @@
+#'@importFrom httr POST
+#'@export 
+authenticateUser <- function(username, password){
+  
+  
+  if(missing(username)){
+    stop('username required for authentication')
+  }
+  
+  if(!interactive() & missing(password)){
+    stop('No password supplied to authenticate_sciencebase in a non-interactive session.')
+  }else{
+    password = ifelse(missing(password), readPassword('Please enter your Active Directory password:'), password)
+  }
+  
+  
+    #https://cida-eros-authdev.er.usgs.gov:8443/auth-webservice/auth/ad/token/
+  ## authenticate
+  resp = POST('https://internal.cida.usgs.gov/auth-webservice/auth/ad/token/', accept_json(),
+              body = list(username=username, password=password), encode='form', config=list(ssl.verifypeer = FALSE))
+  return(content(resp)$tokenId)
+}
+
+readPassword <- function(prompt) {
+  if (exists(".rs.askForPassword")) {
+    pass <- .rs.askForPassword(prompt)
+  } else {
+    pass <- readline(prompt)
+  }
+  return (pass)
+}
+
--- R/extremes.R
+++ R/extremes.R
@@ -1,10 +1,50 @@
+# #'@export
+# extremes <- function(data, output, auth = NULL){
+#   rmd_file <- system.file('extdata','extremes.Rmd',package = 'repgen')
+#   
+#   url = 'https://nwissddvasvis01.cr.usgs.gov/service/timeseries/reports/extremes/?station=06899500&dischargeIdentifier=Discharge.TS0058&stageIdentifier=Gage+height.TS0005&dailyDischargeIdentifier=Discharge.TS0058&waterYear=2012'
+#   auth = 'Bearer aasdfa-asdfasdf-wdfe'
+#   knit('vignettes/extremes.Rmd')
+#   # use knit pandoc ...
+#   system("pandoc -s extremes.md -o extremes.pdf")
+#   system("pandoc -s extremes.md -o extremes.html")
+#   
+#   return(filename)
+# }
+#"username" #"password"
+
+# application/x..codeencoded-url
+
+#post to https://internal.cida.usgs.gov/auth-webservice/token
+#'@title extremes report
+#'@param data
+#'@param output
+#'@param ... additional arguments (e.g., username & password for authentication)
+#'@rdname extremes
 #'@export
-extremes <- function(data, output, auth = NULL){
-  rmd_file <- system.file('extdata','extremes.Rmd',package = 'repgen')
-  
-  url = 'https://nwissddvasvis01.cr.usgs.gov/service/timeseries/reports/extremes/?station=06899500&dischargeIdentifier=Discharge.TS0058&stageIdentifier=Gage+height.TS0005&dailyDischargeIdentifier=Discharge.TS0058&waterYear=2012'
-  auth = 'Bearer aasdfa-asdfasdf-wdfe'
-  knit('vignettes/extremes.Rmd')
-  system("pandoc -s extremes.md -o extremes.pdf")
-  system("pandoc -s extremes.md -o extremes.html")
-}
+setGeneric(name="extremes",def=function(data, output, ...){standardGeneric("extremes")})
+
+setMethod("extremes", signature = c("list", "character"), 
+          definition = function(data, output) {
+            cat('this function does all the work')
+            
+            # elements of data are now in memory, will be used to knit w/ report
+            rmd_file <- system.file('extdata','extremes.Rmd',package = 'repgen')
+            ts <- data
+            knit('vignettes/extremes.Rmd', output = output)
+            
+            return(output)
+          }
+)
+
+#'@importFrom httr GET
+setMethod("extremes", signature = c("character", "character"), 
+          definition = function(data, output,...) {
+            cat('this function authenticates, goes out and gets data, and then calls the other one\n')
+            
+            token <- paste0('Bearer ', authenticateUser(...))
+            browser()
+            ts_list <- getJSON(url = data, auth = token)
+            extremes(ts_list,'output.html')
+          }
+)
