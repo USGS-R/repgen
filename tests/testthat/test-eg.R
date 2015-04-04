c9bc4a23169b0fc0bba47a794f3433b02e2759b0
Jordan S Read <jread@usgs.gov>
2015-04-04 09:54:17 -0500

    updated tests

--- tests/testthat/test-eg.R
+++ tests/testthat/test-eg.R
@@ -1,3 +1,7 @@
+
+context("setting wd to temp folder")
+wd <- getwd()
+setwd(dir = tempdir())
 context("testing extremes json parsing")
 
 test_that("bad list for extremes", {
@@ -15,3 +19,28 @@ test_that('missing data returns as expected', {
   expect_is(getRatingShifts(data, 'shiftPoints', as.numeric = F), 'character')
   expect_error(repgen:::getLims(NA,NA,NA,NA)) 
 })
+
+context("testing extremes when some fields are missing and are complete")
+test_that("example data extremes", {
+  library(jsonlite)
+  data <- fromJSON(system.file('extdata',"extremes-bad.json",package = 'repgen'))
+  expect_is(extremes(data, 'html'), 'character')
+  expect_is(extremes(data, 'pdf'), 'character')
+  data <- fromJSON(system.file('extdata',"extremes-example.json",package = 'repgen'))
+  expect_is(extremes(data, 'html'), 'character')
+  expect_is(extremes(data, 'pdf'), 'character')
+  
+})
+
+context("testing vdiagram when some fields are missing and are complete")
+test_that("example data vdiagram", {
+  library(jsonlite)
+  data <- fromJSON(system.file('extdata',"vdiagram-bad.json",package = 'repgen'))
+  expect_is(vdiagram(data, 'html'), 'character')
+  expect_is(vdiagram(data, 'pdf'), 'character')
+  data <- fromJSON(system.file('extdata',"vdiagram-example.json",package = 'repgen'))
+  expect_is(vdiagram(data, 'html'), 'character')
+  expect_is(vdiagram(data, 'pdf'), 'character')
+  
+})
+setwd(dir = wd)
