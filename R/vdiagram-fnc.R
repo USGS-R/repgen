d9d5b81c93b25bde3808224e7e2bdde6d2e817c6
mike-stern <mwstern@usgs.gov>
2015-05-19 15:33:51 -0500

    AQCU-312 Also label right y-axis, same as left y-axis

--- R/vdiagram-fnc.R
+++ R/vdiagram-fnc.R
@@ -11,9 +11,10 @@ set_up_plot <- function(lims) {
   mj_tck = 10
   mj_tkL = 0.01
   
-  
+  yText = "Stage, in feet"
   # main plot area
-  plot(type="n", x=NA, y=NA, xlim=xaxis, ylim=yaxis, xlab="Shift, in feet", ylab="Stage, in feet", xaxt="n", yaxt="n", mgp=mgp$y)
+  plot(type="n", x=NA, y=NA, xlim=xaxis, ylim=yaxis, xlab=yText, ylab="Stage, in feet", xaxt="n", yaxt="n", mgp=mgp$y)
+  mtext(yText, side=4, line=1)
   actualX = par("usr")[1:2]
   actualY = par("usr")[3:4]
   
@@ -24,12 +25,14 @@ set_up_plot <- function(lims) {
   # minor axes
   axis(side=1, at=pretty(xaxis,mn_tck), tck=mn_tkL, labels=FALSE)
   axis(side=2, at=pretty(xaxis,mn_tck), tck=mn_tkL, labels=FALSE)
+  axis(side=4, at=pretty(xaxis,mn_tck), tck=mn_tkL, labels=FALSE)
   
   # major axes
   majorX = pretty(xaxis,mj_tck)
   majorY = pretty(yaxis,mj_tck)
   axis(side=1, at=majorX, cex.axis=0.5, tck=mj_tkL, mgp=mgp$x, labels=sprintf("%.2f", majorX))
   axis(side=2, at=majorY, cex.axis=0.5, las=2, tck=mj_tkL, mgp=mgp$y, labels=sprintf("%.2f", majorY))
+  axis(side=4, at=majorY, cex.axis=0.5, las=2, tck=mj_tkL, mgp=mgp$y, labels=sprintf("%.2f", majorY))
   
   # edges
   axis(side=1, at=actualX, cex.axis=0.5, tck=0, mgp=mgp$x, labels=sprintf("%.2f", actualX))
