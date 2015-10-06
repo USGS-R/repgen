36f60b9ac16d2d0be8f909417f34dced4a6ba70d
Megan Hines <mhines@usgs.gov>
2015-10-06 10:31:34 -0500

    improving if length of dataset check for sitevisit peak

--- R/sitevisitpeak-data.R
+++ R/sitevisitpeak-data.R
@@ -17,97 +17,89 @@
 
 
 sitevisitpeakTable <- function(data){
-  if(length(data) != 0) {
-    columnNames <- c("Date",
-                     "Time",
-                     "Party",
-                     "Sublocation",
-                     "Verification Method",
-                     "Reading",
-                     "Uncertainty",
-                     "Estimated Date",
-                     "Verification Comments",
-                     "Corrected Value",
-                     "Qualifier",
-                     "Date",
-                     "Time",
-                     "Difference from Peak Verification Reading")
-    
-    #Sends in list of readings, and gets pack the formatted data.frame
-    results <- formatSVPData(data$readings,columnNames)
-    
-    return(results)
-  }
-  if (length(data)==0) {
-    return("The dataset requested is empty")
-  }
+  if (length(data)==0) return ("The dataset requested is empty.")
+  columnNames <- c("Date",
+                   "Time",
+                   "Party",
+                   "Sublocation",
+                   "Verification Method",
+                   "Reading",
+                   "Uncertainty",
+                   "Estimated Date",
+                   "Verification Comments",
+                   "Corrected Value",
+                   "Qualifier",
+                   "Date",
+                   "Time",
+                   "Difference from Peak Verification Reading")
+  
+  #Sends in list of readings, and gets pack the formatted data.frame
+  results <- formatSVPData(data$readings,columnNames)
+  
+  return(results)
 }
 
 formatSVPData <- function(data, columnNames){
-  if(length(data) != 0) {
-    toRet = data.frame(stringsAsFactors = FALSE)
-    for(listRows in row.names(data)){
-      listElements <- data[listRows,]
-      
-      dateTime <- (strsplit(listElements$time, split="[T]"))
-      date <- strftime(dateTime[[1]][1], "%m/%d/%Y")
-      
-      #Break apart, format dates/times, put back together.
-      timeFormatting <- sapply(dateTime[[1]][2], function(s) strsplit(s,split="[-]")[[1]])
-      timeFormatting[[1]] <- sapply(timeFormatting[[1]], function(s) sub(".000","",s))
-      timeFormatting[[2]] <- paste(" (UTC",timeFormatting[[2]], ")")
-      timeFormatting <-  paste(timeFormatting[[1]],timeFormatting[[2]])
-      
-      if(!is.null(listElements$estimatedTime) && !is.na(listElements$estimatedTime)) {
-        estDateTime <- (strsplit(listElements$estimatedTime, split="[T]"))
-        estDate <- strftime(estDateTime[[1]][1], "%m/%d/%Y")
-      } else {
-        estDate <- ""
-      }
-      
-      
-      if(!is.null(listElements$associatedIvTime) && !is.na(listElements$associatedIvTime)) {
-        ivDateTime <- (strsplit(listElements$associatedIvTime, split="[T]"))
-        ivDate <- strftime(ivDateTime[[1]][1], "%m/%d/%Y")
-        
-        ivTimeFormatting <- sapply(ivDateTime[[1]][2], function(s) strsplit(s,split="[-]")[[1]])
-        ivTimeFormatting[[1]] <- sapply(ivTimeFormatting[[1]], function(s) sub(".000","",s))
-        ivTimeFormatting[[2]] <- paste(" (UTC",ivTimeFormatting[[2]], ")")
-        ivTimeFormatting <-  paste(ivTimeFormatting[[1]],ivTimeFormatting[[2]])
-      } else {
-        ivDate <- ""
-        ivTimeFormatting <- ""
-      }
-      
-      quals <- getQualifiers(listElements$associatedIvTime, listElements$associatedIvQualifiers)
-      
-      diff <- getIvDifference(listElements$value, listElements$associatedIvValue)
-      
-      toAdd = c(date,
-                timeFormatting,
-                nullMask(listElements$party), 
-                nullMask(listElements$sublocation), 
-                nullMask(listElements$monitoringMethod), 
-                nullMask(listElements$value),
-                nullMask(listElements$uncertainty), 
-                estDate, 
-                nullMask(listElements$comments),
-                nullMask(listElements$associatedIvValue),
-                quals,
-                ivDate,
-                ivTimeFormatting,
-                diff)
-      
-      toRet <- rbind(toRet, data.frame(t(toAdd),stringsAsFactors = FALSE))
-    }
-    colnames(toRet) <- columnNames
-    rownames(toRet) <- NULL
+  if (length(data)==0) return ("The dataset requested is empty.")
+  toRet = data.frame(stringsAsFactors = FALSE)
+  for(listRows in row.names(data)){
+    listElements <- data[listRows,]
     
-    return(toRet)
-  }  
-  if(length(data) == 0) {
-    return("The dataset requested is empty") 
+    dateTime <- (strsplit(listElements$time, split="[T]"))
+    date <- strftime(dateTime[[1]][1], "%m/%d/%Y")
+    
+    #Break apart, format dates/times, put back together.
+    timeFormatting <- sapply(dateTime[[1]][2], function(s) strsplit(s,split="[-]")[[1]])
+    timeFormatting[[1]] <- sapply(timeFormatting[[1]], function(s) sub(".000","",s))
+    timeFormatting[[2]] <- paste(" (UTC",timeFormatting[[2]], ")")
+    timeFormatting <-  paste(timeFormatting[[1]],timeFormatting[[2]])
+    
+    if(!is.null(listElements$estimatedTime) && !is.na(listElements$estimatedTime)) {
+      estDateTime <- (strsplit(listElements$estimatedTime, split="[T]"))
+      estDate <- strftime(estDateTime[[1]][1], "%m/%d/%Y")
+    } else {
+      estDate <- ""
+    }
+    
+    
+    if(!is.null(listElements$associatedIvTime) && !is.na(listElements$associatedIvTime)) {
+      ivDateTime <- (strsplit(listElements$associatedIvTime, split="[T]"))
+      ivDate <- strftime(ivDateTime[[1]][1], "%m/%d/%Y")
+      
+      ivTimeFormatting <- sapply(ivDateTime[[1]][2], function(s) strsplit(s,split="[-]")[[1]])
+      ivTimeFormatting[[1]] <- sapply(ivTimeFormatting[[1]], function(s) sub(".000","",s))
+      ivTimeFormatting[[2]] <- paste(" (UTC",ivTimeFormatting[[2]], ")")
+      ivTimeFormatting <-  paste(ivTimeFormatting[[1]],ivTimeFormatting[[2]])
+    } else {
+      ivDate <- ""
+      ivTimeFormatting <- ""
+    }
+    
+    quals <- getQualifiers(listElements$associatedIvTime, listElements$associatedIvQualifiers)
+    
+    diff <- getIvDifference(listElements$value, listElements$associatedIvValue)
+    
+    toAdd = c(date,
+              timeFormatting,
+              nullMask(listElements$party), 
+              nullMask(listElements$sublocation), 
+              nullMask(listElements$monitoringMethod), 
+              nullMask(listElements$value),
+              nullMask(listElements$uncertainty), 
+              estDate, 
+              nullMask(listElements$comments),
+              nullMask(listElements$associatedIvValue),
+              quals,
+              ivDate,
+              ivTimeFormatting,
+              diff)
+    
+    toRet <- rbind(toRet, data.frame(t(toAdd),stringsAsFactors = FALSE))
   }
+  colnames(toRet) <- columnNames
+  rownames(toRet) <- NULL
+  
+  return(toRet)
 }
 
 nullMask <- function(val) {
