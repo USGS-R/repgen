7ca24d0c9a80cb73f1121f0013ab406d42472579
Megan Hines <mhines@usgs.gov>
2015-10-07 13:15:19 -0500

    using htmlTable to add column grouping for html version of svp

--- R/sitevisitpeak-data.R
+++ R/sitevisitpeak-data.R
@@ -31,8 +31,8 @@ sitevisitpeakTable <- function(data){
                    "||",
                    "Corrected Value",
                    "Qualifier",
-                   "Date",
-                   "Time",
+                   "Date ",
+                   "Time ",
                    "Difference from Peak Verification Reading")
   
   #Sends in list of readings, and gets pack the formatted data.frame
@@ -85,7 +85,7 @@ formatSVPData <- function(data, columnNames){
               timeFormatting,
               nullMask(listElements$party), 
               nullMask(listElements$sublocation), 
-               "||",
+              "||",
               nullMask(listElements$monitoringMethod), 
               nullMask(listElements$value),
               nullMask(listElements$uncertainty),
