cd380650e0c1753b938045b88f7d000d1a3d851f
Dominick Pattermann <dpattermann@usgs.gov>
2015-12-18 16:43:52 -0600

    Added a new json file (old one is very outdated). Fixed sensorReading-data corrections so that they are not immediately skipped. Don't name variables data.

--- R/sensorreading-data.R
+++ R/sensorreading-data.R
@@ -136,18 +136,16 @@ nullMask <- function(val) {
 
 #calculate the recorder w/in uncertainty
 getRecorderWithinUncertainty <- function(uncertainty, value, recorderValue) {  
-  if ("uncertainty" %in% names(data) && "value" %in% names(data) && "recorderValue" %in% names(data)) {  
-    if (!is.null(uncertainty) && !is.na(uncertainty) && !is.null(value) && !is.na(value)) {
-      ref <- as.numeric(value)
-      unc <- as.numeric(uncertainty)
-      rec <- as.numeric(recorderValue)
-      val1 <- ref+unc
-      val2 <- ref-unc
-      if ((rec<val1) && (rec>val2)) {
-        recorderWithin <- "Yes"
-      } else {
-        recorderWithin <- "No"
-      }
+  if (!is.null(uncertainty) && !is.na(uncertainty) && !is.null(value) && !is.na(value)) {
+    ref <- as.numeric(value)
+    unc <- as.numeric(uncertainty)
+    rec <- as.numeric(recorderValue)
+    val1 <- ref+unc
+    val2 <- ref-unc
+    if ((rec<val1) && (rec>val2)) {
+      recorderWithin <- "Yes"
+    } else {
+      recorderWithin <- "No"
     }
   } else {
     recorderWithin <- ""
@@ -157,12 +155,10 @@ getRecorderWithinUncertainty <- function(uncertainty, value, recorderValue) {
 
 #calculate indicated correction
 getIndicatedCorrection <- function(recorderValue, value) {
-  if ("recorderValue" %in% names(data) && "value" %in% names(data)) {  
-    if ((!is.null(recorderValue)) && (!is.null(value))) {
-      rec <- as.numeric(recorderValue)
-      ref <- as.numeric(value)
-      indicatedCorrection <- round(rec-ref, 2)
-    }
+  if ((!is.null(recorderValue)) && (!is.null(value)) && (!is.na(recorderValue)) && (!is.na(value))) {
+    rec <- as.numeric(recorderValue)
+    ref <- as.numeric(value)
+    indicatedCorrection <- round(rec-ref, 2)
   } else {
     indicatedCorrection <- ""
   }
@@ -171,12 +167,10 @@ getIndicatedCorrection <- function(recorderValue, value) {
 
 # get applied correction
 getAppliedCorrection <- function(raw, corrected) {
-  if ("raw" %in% names(data) && "corrected" %in% names(data)) {  
-    if ((!is.null(raw)) && (!is.null(corrected))) {
-      raw <- as.numeric(raw)
-      corrected <- as.numeric(corrected)
-      appliedCorrection <- round(corrected-raw, 2)
-    }
+  if ((!is.null(raw)) && (!is.null(corrected)) && (!is.na(raw)) && (!is.na(corrected))) {
+    raw <- as.numeric(raw)
+    corrected <- as.numeric(corrected)
+    appliedCorrection <- round(corrected-raw, 2)
   } else {
     appliedCorrection <- ""
   }
@@ -184,19 +178,17 @@ getAppliedCorrection <- function(raw, corrected) {
 } 
 
 getCorrectedRef <- function (value, nearestcorrectedValue, uncertainty) {
-  if ("value" %in% names(data) && "nearestcorrectedValue" %in% names(data) && "uncertainty" %in% names(data)) {
-    if ((!is.null(value)) && (!is.na(value)) && (!is.null(uncertainty)) && (!is.na(uncertainty))) {
-      value <- as.numeric(value) 
-      nearest <- as.numeric(nearestcorrectedValue) 
-      unc <- as.numeric(uncertainty)
-      lower <- value-unc 
-      upper <- value+unc 
-      if ((lower<=nearest) && (upper>=nearest)) { 
-        correctedRef <- "Yes"
+  if ((!is.null(value)) && (!is.na(value)) && (!is.null(uncertainty)) && (!is.na(uncertainty))) {
+    value <- as.numeric(value) 
+    nearest <- as.numeric(nearestcorrectedValue) 
+    unc <- as.numeric(uncertainty)
+    lower <- value-unc 
+    upper <- value+unc 
+    if ((lower<=nearest) && (upper>=nearest)) { 
+      correctedRef <- "Yes"
       }
-      else {
-        correctedRef <- "No"
-      }
+    else {
+      correctedRef <- "No"
     }
   } else {
     correctedRef <- ""
