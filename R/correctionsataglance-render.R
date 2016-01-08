8df8073262df91fb48e4f57f5bc9198e9ca1279d
Lindsay Carr <lcarr@usgs.gov>
2016-01-08 12:29:03 -0600

    add pre-processing boxes to plot

--- R/correctionsataglance-render.R
+++ R/correctionsataglance-render.R
@@ -18,14 +18,23 @@ correctionsataglanceReport <- function(data){
     #approvals at top bar
     rect(xleft = parseData$apprData$startDates,
          xright = parseData$apprData$endDates,
-         ybottom = rep(95, parseData$apprData$apprNum),
-         ytop = rep(100, parseData$apprData$apprNum), 
+         ybottom = rep(95, parseData$apprData$dataNum),
+         ytop = rep(100, parseData$apprData$dataNum), 
          col = parseData$apprData$apprCol) %>% 
     
     #field visit points
     points(x = as.POSIXct(data$fieldVisits$startTime), 
            y=rep(92, length(data$fieldVisits$startTime)), 
-           pch=24, col="black", bg="grey")
+           pch=24, col="black", bg="grey") %>% 
+    
+    #pre-processing corrections
+    rect(xleft = parseData$preproData$startDates,
+         xright = parseData$preproData$endDates,
+         ybottom = rep(85, parseData$preproData$dataNum),
+         ytop = rep(90, parseData$preproData$dataNum)) %>% 
+    text(x = parseData$preproData$xyText$x, 
+         y = parseData$preproData$xyText$y, 
+         labels = parseData$preproData$corrLabel, cex=0.5)
   
   return(timeline)
   
