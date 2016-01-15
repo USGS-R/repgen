d15781a098a99a88f7c6079e9e6eba8f6747da42
Lindsay Carr <lcarr@usgs.gov>
2016-01-15 14:28:31 -0600

    order dates by applied date -- applied date is priority for shifting corrections to new lane

--- R/correctionsataglance-data.R
+++ R/correctionsataglance-data.R
@@ -188,8 +188,14 @@ findOverlap <- function(dataList){
   fixLines <- lapply(dataList, function(dataIn) {
     if(!is.null(dataIn)){
       new_line <- FALSE
-      if(dataIn$dataNum > 1){
-        lane <- 1
+      if(dataIn$dataNum > 1){ 
+        
+        #ordered by applied date
+        dates_df <- as.data.frame(dataIn[-dataIn$dataNum], stringsAsFactors = FALSE)
+        dates_df <- dates_df[order(dataIn.applyDates),]
+        dates_list <- as.list(dates_df)
+        dataIn <- append(dates_list, list(dataNum = dataIn$dataNum))
+        
         for(n in 2:dataIn$dataNum){
           before_n <- seq((n-1))
           is_overlap <- dataIn$startDate[n] < dataIn$endDate[before_n]
