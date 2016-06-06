81e6b99a86f26f9d152285228a2e4f769f975424
Lindsay Carr <lcarr@usgs.gov>
2016-06-06 15:56:46 +0000

    uvhydro: chart uncorr to corr values

--- R/uvhydrograph-data.R
+++ R/uvhydrograph-data.R
@@ -13,7 +13,8 @@ parseUVData <- function(data, plotName, month) {
     
     corr_UV <- subsetByMonth(getUvHydro(data, "primarySeries" ), month)
     est_UV <- subsetByMonth(getUvHydro(data, "primarySeries", estimatedOnly=TRUE), month)
-    uncorr_UV <- subsetByMonth(getUvHydro(data, "primarySeriesRaw" ), month)
+    uncorr_UV <- subsetByMonth(getUvHydro(data, "primarySeriesRaw" ), month) %>% 
+      omitRawExtremes(correctedData = corr_UV)
     comp_UV <- subsetByMonth(getUvHydro(data, "comparisonSeries" ), month)
     water_qual <- subsetByMonth(getWaterQualityMeasurements(data), month)
     
@@ -314,4 +315,11 @@ getInverted <- function(data, renderName, plotName) {
   
   isInverted <- ifelse(!is.null(dataName), data[[dataName]][['inverted']], NA)
   return(isInverted)
+}
+
+omitRawExtremes <- function(rawData, correctedData){
+  rawData_to_plot <- left_join(correctedData, rawData, by = "time") %>% 
+    select(-value.x, -month.x) %>% 
+    rename(value = value.y, month = month.y)
+  return(rawData_to_plot)
 }
