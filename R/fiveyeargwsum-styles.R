495bc0228e3f90c2abd898ef972279c4362478c8
Megan Hines <mhines@usgs.gov>
2016-08-24 12:49:49 -0500

    updates for fiveyear and bottom approval bar, faking json fixes in example file ahead of https://internal.cida.usgs.gov/jira/browse/AQCU-809

--- R/fiveyeargwsum-styles.R
+++ R/fiveyeargwsum-styles.R
@@ -11,9 +11,9 @@ getFiveyearStyle <- function(data, info=NULL, ...) {
                      max_iv = list(points = list(x=x, y=y, col="red", pch=8, cex=2, legend.name=paste(args$maxLabel, info$type, ":", y))),
                      min_iv = list(points = list(x=x, y=y, col="blue", pch=8, cex=2, legend.name=paste(args$minLabel, info$type, ":", y))),
                      gw_level = list(points = list(x=x,y=y, pch = 8, bg = 'orange', col = 'orange', cex = 1.2, lwd=1, legend.name="Measured Water Level (NWIS-RA)")),
-                     appr_approved = list(points = list(x=x, y=y, col="lightskyblue", type='p', pch=data[[1]]$point_type, cex=2.5, bg="lightskyblue", legend.name=legend.name, where='first')),
-                     appr_inreview = list(points = list(x=x, y=y, col="yellow2", type='p', pch=data[[1]]$point_type, cex=2.5, bg="yellow2", legend.name=legend.name, where='first')),
-                     appr_working = list(points = list(x=x, y=y, col="lightpink", type='p', pch=data[[1]]$point_type, cex=2.5, bg="lightpink", legend.name=legend.name, where='first'))
+                     appr_approved_uv = list(rect = list(xleft=data[[1]]$x0, xright=data[[1]]$x1, ybottom=data[[1]]$y0, ytop=data[[1]]$y1, col="lightskyblue", border="lightskyblue", legend.name=legend.name, where='first')),
+                     appr_inreview_uv = list(rect = list(xleft=data[[1]]$x0, xright=data[[1]]$x1, ybottom=data[[1]]$y0, ytop=data[[1]]$y1, col="yellow2", border="yellow2", legend.name=legend.name, where='first')),
+                     appr_working_uv = list(rect = list(xleft=data[[1]]$x0, xright=data[[1]]$x1, ybottom=data[[1]]$y0, ytop=data[[1]]$y1, col="lightpink", border="lightpink", legend.name=legend.name, where='first'))
                      )
     
     return(styles)
