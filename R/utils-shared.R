d2a11b176579a9d9d4abe8db8d6b9a8d483fa777
Lindsay Carr <lcarr@usgs.gov>
2016-09-01 10:53:50 -0500

    reverting isEmptyVar to use "all"

--- R/utils-shared.R
+++ R/utils-shared.R
@@ -213,9 +213,10 @@ isEmptyOrBlank <- function(val = NULL, listObjects = NULL, objectName = NULL){
 ############ used in uvhydrograph-data, dvhydrograph-data, fiveyeargwsum-data ############ 
 
 isEmptyVar <- function(variable){
-  any(is.null(variable), 
-      nrow(variable) == 0 || is.null(nrow(variable)), 
-      length(variable$time[!is.na(variable$time)]) == 0)
+  result <- all(is.null(variable), 
+                nrow(variable) == 0 || is.null(nrow(variable)), 
+                length(variable$time[!is.na(variable$time)]) == 0)
+  return(result)
 }
 
 #' if there are gaps in the timeseries, don't connect them
