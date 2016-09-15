6765057013a0f29b4dc783503d55f36414183685
Andrew Halper <ashalper@usgs.gov>
2016-09-15 11:39:01 -0700

    Removed y0, y1 setting code in getApprovals():
    ...as it needs to be done later on.

--- R/utils-json.R
+++ R/utils-json.R
@@ -286,8 +286,6 @@ getApprovals <- function(data, chain_nm, legend_nm, appr_var_all, month=NULL, po
         approval_info[[i]] <- list(
           x0 = appr_dates[i, 1],
           x1 = appr_dates[i, 2],
-          y0 = substitute(ApprovalInfoY(plot_object, 1, 0.04)),
-          y1 = substitute(ApprovalInfoY(plot_object, 1, 0.0245)),
           legend.name = paste(appr_dates[i, 4], legend_nm),
           time = appr_dates[1, 1]
         ) # added a fake time var to get through a future check
@@ -305,48 +303,6 @@ subsetByMonth <- function(pts, onlyMonth) {
     return(subset(pts, month == onlyMonth))
   }
   return(pts)
-}
-
-#' Compute top and bottom vertical position of approval bars.
-#' @param object A gsplot object.
-#' @param times "times" argument of R rep() function.
-#' @param ratio A scaling ratio to adjust top or bottom of approval bar rectangle.
-#' @return Approval bar, top or bottom y-axis point, in world coordinates.
-ApprovalInfoY <- function(object, times, ratio) {
-  reverse <- object$side.2$reverse
-
-  if (is.null(object$global$par$ylog)) {
-    # presume the semantics of NULL as FALSE, which may or not be correct, but 
-    # keeps the code from terminating here
-    log <- FALSE
-  }
-  else {
-    log <- object$global$par$ylog
-  }
-
-  e.0 <- object$side.2$lim[1]
-  e.1 <- object$side.2$lim[2]
-  
-  # if this is a log10 y-axis
-  if (log) {
-    # if y-axis is inverted
-    if (reverse) {
-      y <- 10^(log10(e.1) + ratio * (log10(e.1) - log10(e.0)))
-    }
-    else {
-      y <- 10^(log10(e.0) - ratio * (log10(e.1) - log10(e.0)))
-    }
-  }
-  else {
-    if (reverse) {
-      y <- e.1 + ratio * (e.1 - e.0)
-    }
-    else {
-      y <- e.0 - ratio * (e.1 - e.0)
-    }
-  }
-
-  return(rep(y, times))
 }
 
 getApprovalDates <- function(data, chain_nm, approval){
