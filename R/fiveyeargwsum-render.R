a446a7691fa6540969eb3e94f01edeecf1f10b80
Andrew Halper <ashalper@usgs.gov>
2016-09-19 09:26:47 -0700

    Corrected vertical position of approval bars.

--- R/fiveyeargwsum-render.R
+++ R/fiveyeargwsum-render.R
@@ -23,16 +23,24 @@ createfiveyeargwsumPlot <- function(data){
     
     fiveyrInfo <- parseFiveYrSupplemental(data, fiveyrData)
     
-    plot_object <- gsplot(yaxs='r', xaxt="n", mar=c(8, 4, 4, 2) + 0.1) %>% 
-      axis(side=1, at=fiveyrInfo$date_seq_mo, labels=FALSE) %>%
-      view(xlim=c(fiveyrInfo$startDate, fiveyrInfo$endDate)) %>%
-      mtext(text=fiveyrInfo$month_label, at=fiveyrInfo$month_label_location, cex=0.5, side=1) %>% 
-      mtext(text=year(fiveyrInfo$date_seq_yr), at=fiveyrInfo$date_seq_yr+(60*60*24*30*6), line=1, side=1) %>% 
-      legend(location="below", cex=0.8, ncol=2, y.intersp=1.5) %>% 
-      axis(side=2, reverse=isInverted) %>% 
-      grid(col="lightgrey", lty=1) %>% 
-      title(main=data$reportMetadata$title,
-            ylab="Water Level, Below LSD (feet)")
+    plot_object <- gsplot(yaxs = 'i', xaxt = "n", mar = c(8, 4, 4, 2) + 0.1) %>%
+      axis(side = 1, at = fiveyrInfo$date_seq_mo, labels = FALSE) %>%
+      view(xlim = c(fiveyrInfo$startDate, fiveyrInfo$endDate)) %>%
+      mtext(
+        text = fiveyrInfo$month_label,
+        at = fiveyrInfo$month_label_location,
+        cex = 0.5, side = 1
+      ) %>%
+      mtext(
+        text = year(fiveyrInfo$date_seq_yr),
+        at = fiveyrInfo$date_seq_yr + (60 * 60 * 24 * 30 * 6),
+        line = 1, side = 1
+      ) %>%
+      legend(
+        location = "below", cex = 0.8, ncol = 2, y.intersp = 1.5) %>%
+      axis(side = 2, reverse = isInverted) %>%
+      grid(col = "lightgrey", lty = 1) %>%
+      title(main = data$reportMetadata$title, ylab = "Water Level, Below LSD (feet)")
     
     for (i in grep("^appr_.*_uv$", names(fiveyrData), invert = TRUE)) {
       fiveyrStyles <- getFiveyearStyle(fiveyrData[i], fiveyrInfo, maxLabel = maxLabel, minLabel = minLabel)
@@ -41,13 +49,15 @@ createfiveyeargwsumPlot <- function(data){
       }
     }
     
-    plot_object <- ApplyApprovalBarStyles(plot_object, fiveyrData)
-    
     plot_object <- rm.duplicate.legend.items(plot_object)
     
     plot_object <- plot_object %>% 
       abline(v=fiveyrInfo$date_seq_yr, col="gray47", lwd=2, where='first')
     
+    plot_object <- ApplyApprovalBarStyles(plot_object, fiveyrData)
+    
+    # patch up top extent of y-axis
+    plot_object <- RescaleYTop(plot_object)
   }
   else {
     plot_object <- NULL
