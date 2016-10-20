848f89fab3e48b56885e71daac5277c69297fcf8
Andrew Halper <ashalper@usgs.gov>
2016-10-20 07:48:26 -0700

    Restored a new-and-improved version of RescaleYTop().

--- R/utils-shared.R
+++ R/utils-shared.R
@@ -518,6 +518,56 @@ ApprovalBarY <- function(lim, ylog = NULL, reverse, ratio) {
   return(y)
 }
 
+#' Rescale top of y-axis to create ~4% margin between vertical top extent of 
+#' plot objects and top edge of plot. This is an inaccurate emulation of (the 
+#' top-end-of-plot behavior of) R graphics::par's "yaxs = 'r'" state, because we
+#' have to use "yaxs = 'i'" in spots, but still want the ~4% margin at the top 
+#' of the plot, so we adjust the y-axis endpoint accordingly after we do what we
+#' need.
+#' @param object A gsplot, plot object.
+#' @return The passed-in gsplot object, with y-axis top augmented (upwards).
+RescaleYTop <- function(object) {
+  ylog <- par("ylog")
+  reverse <- object$side.2$reverse
+  
+  # Desired top margin, in NDCs. See also "yaxs" parameter domain in
+  # graphics::par.
+  m <- 0.04
+  
+  # vertical extent and length
+  e <- ylim(object)$side.2
+  e.length <- abs(e[1] - e[2])
+  
+  if (ylog) {
+    # TODO: the log10 calculations below should probably be upgraded to mirror
+    # the form of analogous formulae in the linear-case, conditionals in the
+    # block below.
+    
+    # if the y-axis is inverted
+    if (reverse) {
+      object$side.2$lim[1] <- 10^((1 - m) * log10(e[2]))
+    }
+    else {
+      object$side.2$lim[2] <- 10^((1 + m) * log10(e[2]))
+    }
+  }
+  else {
+    # if the y-axis is inverted
+    if (reverse) {
+      object$side.2$lim[1] <- e[2] - m * e.length
+    }
+    else {
+      # The 5.14 coefficient below is a hack that was reverse-engineered from 
+      # the SVG output (using Inkscape, and the "back-of-the-envelope"). At the 
+      # moment, we have no idea why it works, and is likely not robust enough 
+      # for production.
+      object$side.2$lim[2] <- e[2] + 5.14 * m * e.length
+    }
+  }
+  
+  return(object)
+}
+
 #Will clear out any folders and files in temp folder that are older than 5 minutes
 cleanTempSpace <- function() {
   tempdir <- dirname(tempfile())
