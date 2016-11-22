3a23fd96fbe216a7783d16654bc26dba73aacae6
Megan Hines <mhines@usgs.gov>
2016-11-22 09:57:55 -0600

    cleaning up check, missing library calls in extremes test

--- tests/testthat/test-extremes.R
+++ tests/testthat/test-extremes.R
@@ -5,8 +5,10 @@ setwd(dir = tempdir())
 
 context("testing extremes json parsing")
 test_that("bad list for extremes", {
-    data <- list('data'=c(0,0,0))
-    expect_error(extremes(data, 'html'))
+  library(jsonlite)
+  library(dplyr)
+  data <- list('data'=c(0,0,0))
+  expect_error(extremes(data, 'html'))
 })
 
 context("testing extremes when some fields are missing and are complete")
