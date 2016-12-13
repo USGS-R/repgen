dbb3ba97481e1be6f73ccb531c2974aa7ddcaae9
zmoore-usgs <zmoore@usgs.gov>
2016-12-13 09:45:38 -0600

    AQCU-916: Clarifief variable names and documentation. Slightly increased label box size modifier.

--- R/uvhydrograph-data.R
+++ R/uvhydrograph-data.R
@@ -225,10 +225,10 @@ parseLabelSpacing <- function(data, info) {
   
   if (names(data) %in% c("series_corr", "series_corr_ref", "series_corr_up", "series_corr2")){
     limits <- info[[grep("lims_UV", names(info))]]
-    #Number of hours to offset labels by for display
-    hourOffset <- 4 * 60 * 60
+    #Number of seconds to offset labels by for display
+    secondOffset <- 4 * 60 * 60
     #Width of one digit in hours
-    digitHours <- 4 * 60 * 60
+    digitSeconds <- 4 * 60 * 60
     #Total width of both bounding box left and right margins 
     baseBoxSize <- 4 * 60 * 60
     #Minimum space between the right side of a label box and the next correction line to not merge columns
@@ -243,13 +243,13 @@ parseLabelSpacing <- function(data, info) {
     #Propagate the width value to all rows with the current time
     corrs <- corrs %>% group_by(time) %>% mutate(colWidth = cumsum(colWidth)) %>% ungroup()
     #Calculate the size of each label box
-    corrs <- corrs %>% mutate(boxWidth = baseBoxSize + digitHours * colWidth)
+    corrs <- corrs %>% mutate(boxWidth = baseBoxSize + digitSeconds * colWidth)
     #Calculate column breaks based on widths and times
-    corrs <- corrs %>% mutate(newCol = ifelse(row_number() == 1 | difftime(time, lag(time), units="secs") >=  hourOffset + boxWidth + minSpacerSize, TRUE, FALSE))
+    corrs <- corrs %>% mutate(newCol = ifelse(row_number() == 1 | difftime(time, lag(time), units="secs") >=  secondOffset + boxWidth + minSpacerSize, TRUE, FALSE))
     #Calculate the column number of each row by summing up the newCol column
     corrs <- corrs %>% mutate(colNum = cumsum(as.numeric(newCol)))
     #Calculate the x-position of new columns
-    corrs <- corrs %>% arrange(desc(time), desc(label)) %>% mutate(xpos = ifelse(row_number() == 1 | lag(colNum) != colNum, time + hourOffset + boxWidth/2, 0))
+    corrs <- corrs %>% arrange(desc(time), desc(label)) %>% mutate(xpos = ifelse(row_number() == 1 | lag(colNum) != colNum, time + secondOffset + boxWidth/2, 0))
     #Propagate the x-position value to all labels in a particular column
     corrs <- corrs %>% group_by(colNum) %>% mutate(xpos = cumsum(xpos)) %>% arrange(colNum, label)
     #Add a y-offset multiplier value to each row that increases within columns
@@ -259,20 +259,20 @@ parseLabelSpacing <- function(data, info) {
     #Move any x-positions that are off the chart to the left of their location by subtracating double what was added
     corrs <- corrs %>% mutate(shift = ifelse(xpos > limits$xlim[[2]], TRUE, FALSE)) %>% ungroup()
     #Shifted columns should use the earliest date to offset from instead of the latest, so re-calculate xpos from this date
-    corrs <- corrs %>% arrange(colNum, time, label) %>% mutate(xpos = ifelse(shift, ifelse(row_number() == 1 | lag(colNum) != colNum, time - hourOffset - boxWidth/2, 0), xpos))
+    corrs <- corrs %>% arrange(colNum, time, label) %>% mutate(xpos = ifelse(shift, ifelse(row_number() == 1 | lag(colNum) != colNum, time - secondOffset - boxWidth/2, 0), xpos))
     #Propagate the x-position value to all labels in shifted columns
     corrs <- corrs %>% group_by(colNum) %>% mutate(xpos = ifelse(shift, cumsum(xpos), xpos)) %>% ungroup() %>% arrange(colNum, label)
     #If we shifted any columns to the other side check for overlapping columns (this really only matters for the last column)
-    corrs <- corrs %>% mutate(overlap = ifelse(colNum != lag(colNum), ifelse(xpos - lag(xpos) < hourOffset + boxWidth, 1, 0), 0)) %>%
+    corrs <- corrs %>% mutate(overlap = ifelse(colNum != lag(colNum), ifelse(xpos - lag(xpos) < secondOffset + boxWidth, 1, 0), 0)) %>%
                        mutate(overlap = ifelse(is.na(overlap), 0, ifelse(row_number() < n() & colNum != lead(colNum) & lead(overlap) > 0, 1, overlap)))
     #Propagate found overlap to all rows in this column
     corrs <- corrs %>% group_by(colNum) %>% arrange(desc(overlap)) %>% mutate(overlap = cumsum(overlap)) %>% arrange(colNum, desc(time), label) %>% ungroup()
     #Pull overlapping labels back towards their respective lines and stagger them vertically
-    corrs <- corrs %>% mutate(xpos = ifelse(overlap > 0, ifelse(shift, xpos + hourOffset - 1, xpos - hourOffset - 1), xpos)) %>%
+    corrs <- corrs %>% mutate(xpos = ifelse(overlap > 0, ifelse(shift, xpos + secondOffset - 1, xpos - secondOffset - 1), xpos)) %>%
                        mutate(ypos = ifelse(overlap > 0, ifelse(!shift, ifelse(row_number() > 1, ypos - (subtractor * (multiplier-1)), ypos), ypos - (subtractor * (multiplier))), ypos))
 
     ##The scaling factor for the bounding shape of this label in inches. Scaling factor is fairly arbitrary but is relative the cex value used for the text for these labels in the styles and the colWidth
-    corrs <- corrs %>% mutate(r = 1+0.475*nchar(as.character(label)))
+    corrs <- corrs %>% mutate(r = 1+0.5*nchar(as.character(label)))
       
     spacingInfo <- list(x=corrs$xpos, xorigin=corrs$time, y=corrs$ypos, r=corrs$r, label=corrs$label)
   } else {
