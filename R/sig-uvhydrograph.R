6dab9aac2ffb380a5d4808bbce3e7b7392814c45
Megan Hines <mhines@usgs.gov>
2017-01-05 22:16:59 +0000

    deleting anything related to auth also removing sig URL methods removing examples with URLs where auth was required deleting utils-auth and utils-web because they are no longer used

--- R/sig-uvhydrograph.R
+++ R/sig-uvhydrograph.R
@@ -1,6 +1,6 @@
 #'@title UV Hydrograph report
 #'@param data local data (as list) or URL
-#'@param ... additional params passed to GET or authenticateUser
+#'@param ... additional params passed to GET
 #'@rdname uvhydrograph
 #'@importFrom rmarkdown render
 #'@examples
@@ -11,15 +11,6 @@
 #'Sys.setenv(TZ = "UTC")
 #'gwData <- fromJSON(system.file('extdata','uvhydrograph','uvhydro-groundwater.json', package = 'repgen'))
 #'uvhydrograph(gwData, 'Author Name')
-#'\dontrun{
-#' url <- paste0('https://nwissddvasvis01.cr.usgs.gov/service/timeseries/reports/swuvhydrograph/',
-#' '?station=05421682&dischargeIdentifier=Discharge.ft%5E3%2Fs&stageIdentifier=',
-#' 'Gage+height.ft.Work&dailyDischargeIdentifier=Discharge.ft%5E3%2Fs.Mean',
-#' '&ratingModelIdentifier=Gage+height-Discharge.STGQ&waterYear=2011')
-#'
-#'# pass in additional params to authenticateUser
-#'uvhydrograph(url, verbose = TRUE, username = 'bbadger', password = '12345')
-#'uvhydrograph(url, 'Author Name')
 #'}
 #'@rdname uvhydrograph
 #'@export
@@ -33,14 +24,3 @@ setMethod("uvhydrograph", signature = c("list"),
             return(startRender(data, author, 'uvhydrograph'))
           }
 )
-
-#'@aliases uvhydrograph
-#'@rdname uvhydrograph
-setMethod("uvhydrograph", signature = c("character"), 
-          definition = function(data, ...) {
-            data <- getJSON(url = data, ...)
-            uvhydrograph(data)
-          }
-)
-
-
