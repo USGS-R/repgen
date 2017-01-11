1478458b27ac663a57ffed820445c9b3fe189d99
Andrew Halper <ashalper@usgs.gov>
2017-01-11 11:05:38 -0700

    roxygen edits.

--- R/utils-dataFlags.R
+++ R/utils-dataFlags.R
@@ -1,7 +1,7 @@
 #' @title zeroValues
 #' @description Function that returns the data equal to zero.
 #' @param timeSeriesData The time series that is checked for data containing zero
-#' @return A logical value that is true if there is any data that is zero.
+#' @return A logical value that is \code{TRUE} if there is any data that is zero.
 zeroValues <- function(timeSeriesData){ 
   if(class(timeSeriesData) == "list"){
     zeroList <- lapply(timeSeriesData, function(x) {any(na.omit(x[["value"]]) == 0)})
@@ -15,7 +15,7 @@ zeroValues <- function(timeSeriesData){
 #' @title negValues
 #' @description Function that returns negative data
 #' @param timeSeriesData The time series that is checked for negative data
-#' @return A logical value that is true if there are any negative values.
+#' @return A logical value that is \code{TRUE} if there are any negative values.
 negValues <- function(timeSeriesData){    
   if(class(timeSeriesData) == "list"){
     negList <- lapply(timeSeriesData, function(x) {any(na.omit(x[["value"]]) < 0)})
@@ -32,9 +32,8 @@ negValues <- function(timeSeriesData){
 #' 
 #' @param timeseries The timseries to potentially be inverted.
 #' 
-#' @return Returns a logical value that is true if the timeseries should be inverted.
-#' 
-
+#' @return Returns a logical value that is \code{TRUE} if the time series should
+#'   be inverted.
 isTimeSeriesInverted <- function(timeseries) {
   invertedFlag <- timeseries[['inverted']]
   isInverted <- ifelse(is.null(invertedFlag), FALSE, invertedFlag)
@@ -43,15 +42,17 @@ isTimeSeriesInverted <- function(timeseries) {
 
 #' @title isLogged
 #' 
-#' @description To check if a timeseries can be plotted on a logged axis.
+#' @description To check if a time series can be plotted on a logged axis.
 #' 
-#' @param isVolFlow A logical value indicating whether the timeseries measures
-#' a volumetric flow or not.
-#' @param ts_data The timeseries data itself. If this is null but the other params are true, it returns true.
-#' @param excludeZeroNegative A logical value indicating whether the timeseries
-#' will exclude zero and negative values.
+#' @param isVolFlow A logical value indicating whether the time series measures 
+#'   a volumetric flow or not.
+#' @param ts_data The time series data itself. If this is \code{NULL} but the other
+#'   params are \code{TRUE}, it returns \code{TRUE}.
+#' @param excludeZeroNegative A logical value indicating whether the time series
+#'   will exclude zero and negative values.
 #' 
-#' @return Return true if the timeseries can be plotted on a logged axis and false otherwise.
+#' @return Returns \code{TRUE} if the time series can be plotted on a logged
+#'   axis, and \code{FALSE} otherwise.
 #'
 isLogged <- function(ts_data, isVolFlow, excludeZeroNegative){
   
