d11288878b0a81b95c315f8d52c0d0587b807187
Pattermann <dpattermann@usgs.gov>
2017-01-13 13:30:44 -0600

    Add another test to make sure it doesn't change when we don't want it to.

--- tests/testthat/test-utils-limits.R
+++ tests/testthat/test-utils-limits.R
@@ -5,7 +5,7 @@ setwd(dir = tempdir())
 
 
 test_that("lines to callouts", {
-  
+
   g = gsplot::callouts(gsplot::gsplot(),x=3, y=2, labels=c('EVEN LONGER HOLY CATS'), angle=150)
   j = repgen:::testCallouts(g,c(2,3))
   expect_equal(g$view.1.2$callouts$angle,j$view.1.2$callouts$angle)
@@ -14,6 +14,7 @@ test_that("lines to callouts", {
   g = gsplot::callouts(gsplot::gsplot(), x=3, y=2, labels=c('EVEN LONGER HOLY CATS'), angle=250)
   j = repgen:::testCallouts(g,c(2,3))
   expect_false(g$view.1.2$callouts$angle==j$view.1.2$callouts$angle)
+  
 })
 
 
