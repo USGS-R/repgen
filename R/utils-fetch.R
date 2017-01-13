17265d3fb8b6c0eb2f2646c9784cfe3fc1f29f2e
zmoore-usgs <zmoore@usgs.gov>
2017-01-13 12:58:43 -0600

    Added necessary functionality to readTimeSeries and readEstimatedTimeSeries in order to deprecate parseEstimatedStatDerived and getEstimatedDates. Those functions still exist for the same reasons as getTimeSeries and should be removed once all calls to them from the reports are removed. Added warnings to getTimeSeries, parseEstimatedStatDerived, and getEstimatedDates alerting the user that these functions are deprecated and that calls should be switched to readTimeSeries and readEstimatedTimeSeries.

--- R/utils-fetch.R
+++ R/utils-fetch.R
@@ -32,29 +32,6 @@ fetchApprovalsForSeries <- function(reportObject, seriesName){
   return(val)
 }
 
-# used in dvhydrograph and fiveyrgwsum
-parseEstimatedStatDerived <- function(data, points, date_index, legend_nm, chain_nm, estimated){
-  if(estimated){
-    formatted_data <- list(time = points[['time']][date_index],
-                           value = points[['value']][date_index],
-                           legend.name = paste("Estimated", data[['reportMetadata']][[legend_nm]]),
-                           estimated=estimated)
-  } else if(!estimated && length(date_index) != 0) {
-    formatted_data <- list(time = points[['time']][-date_index],
-                           value = points[['value']][-date_index],
-                           legend.name = data[['reportMetadata']][[legend_nm]],
-                           estimated=estimated)
-  } else {
-    formatted_data <- list(time = points[['time']],
-                           value = points[['value']],
-                           legend.name = data[['reportMetadata']][[legend_nm]],
-                           estimated=estimated)
-  }
-  
-  formatted_data$field <- chain_nm
-  return(formatted_data)
-}
-
 #' Fetch Rating Shifts
 #'
 #' @description Given a report object, will attempt to pull the rating shifts list.
