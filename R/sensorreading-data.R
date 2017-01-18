379bcdfc8a770f6cc0dc96fc0604a18dee41c7df
Megan Hines <mhines@usgs.gov>
2017-01-18 14:54:27 -0600

    more documentation/cleanup for srs

--- R/sensorreading-data.R
+++ R/sensorreading-data.R
@@ -166,7 +166,7 @@ formatSensorData <- function(reportData, columnNames, includeComments){
 #' @description This takes the uncertainty values, the reading values and the recorderValues
 #' and returns a Yes or No repsonse if the reading value is within the uncertainty level. 
 #' If the recorderValue is not available, it compares the reading value with the recorderValue
-#' and if the values match, returns Yes, if not, No
+#' and if the values match, returns Yes, if not, No. The uncertainty range is inclusive.
 #' 
 #' @param uncertainty The value for uncertainty for the reading
 #' 
@@ -211,14 +211,16 @@ getRecorderWithinUncertainty <- function(uncertainty, value, recorderValue) {
 
 #' calculate indicated correction
 #' 
-#' @description Takes a recorderValue and a reading value and returns the correction 
+#' @description Takes a recorderValue and a reading value and returns the 
+#' difference between the recorder value and the reference value.
+ 
 #' 
 #' @param recorderValue The recorderValue from the data
 #'
 #' @param value The reading value from the data
 #' 
-#' @return indicatedCorrection A rounded value for correction or an empty character if
-#' the recorderValue and reading value are empty/missing
+#' @return indicatedCorrection A rounded difference value for correction or 
+#' an empty character if the recorderValue and reading value are empty/missing
 #' 
 getIndicatedCorrection <- function(recorderValue, value) {
   if ((!isEmpty(recorderValue)) && (!isEmpty(value))) {
@@ -231,7 +233,17 @@ getIndicatedCorrection <- function(recorderValue, value) {
   return(indicatedCorrection)
 }
 
-# get applied correction
+#' get applied correction
+#' 
+#' @description Takes a raw and corrected value and calculates the applied correction
+#' if they are not null or empty
+#' 
+#' @param raw The raw reading value
+#' 
+#' @param corrected The corrected reading value
+#' 
+#' @return The rounded difference between the raw and corrected values
+#' 
 getAppliedCorrection <- function(raw, corrected) {
   if ((!isEmpty(raw)) && (!isEmpty(corrected))) {
     raw <- as.numeric(raw)
@@ -243,6 +255,22 @@ getAppliedCorrection <- function(raw, corrected) {
   return(appliedCorrection)
 } 
 
+#' Are the corrected values within the reference values accounting for 
+#' their specified uncertainty?
+#' 
+#' @description This function returns a yes or no when comparing whether the 
+#' corrections applied to the recorder reading are within the reference values
+#' accounting for their specified uncertainty?
+#' 
+#' @param value The reading value
+#' 
+#' @param nearestCorrectedValue The nearest corrected value to the reading value
+#'
+#' @param uncertainty The uncertainty specified for the reading value
+#' 
+#' @return Yes or No if the corrections applied are within the reference
+#' values for their specified uncertainty
+#' 
 getCorrectedRef <- function (value, nearestcorrectedValue, uncertainty) {
   if ((!isEmpty(value)) && (!isEmpty(uncertainty)) && (!isEmpty(nearestcorrectedValue))) {
     value <- as.numeric(value) 
@@ -262,6 +290,15 @@ getCorrectedRef <- function (value, nearestcorrectedValue, uncertainty) {
   return(correctedRef)
 }
 
+
+#' Get the qualifiers for use in the main table
+#' 
+#' @description takes the qualifiers and parses them to format for use in the table
+#' 
+#' @param inQualifiers The qualifiers coming in from the JSON data string
+#' 
+#' @return Formatted string of qualifiers 
+#' 
 getSRSQualifiers <- function(inQualifiers) {
   if(length(inQualifiers) < 1) return("");
   q <- inQualifiers[[1]]$code
@@ -285,6 +322,15 @@ getSRSQualifiers <- function(inQualifiers) {
   return(builtQualifiers)
 }
 
+#' Returns a list of comments or an empty character if there are no comments
+#' 
+#' @description Accepts the comments string and checks to see if it's null or empty,
+#' and if it is, returns an empty string
+#' 
+#' @param comments The text comments from the JSON data
+#' 
+#' @return comments as they were passed or an empty string if empty or null
+
 getComments <- function(comments) {
   comm <- unlist(comments)
   if (!isEmptyOrBlank(comm)) {
@@ -295,21 +341,33 @@ getComments <- function(comments) {
   }
   return(value)
 }
-#used simply to set precision for some known numbers rather
-#than having a hardcode precision number sprinkled out.
+
+#' Sets a precision value for some known numbers rather
+#' than having a hardcode precision number sprinkled out.
+#' 
+#' @description Provides a precision value which can be used any time we want to 
+#' control precision of values
+#' 
+#' @return The precision identified in the function
+#' 
 getSrsPrecision <- function() {
   return(2);
 }
 
-#' Create Flat Text, "qualifiers table" Type Output Table
+#' Create a table of qualifiers that are used in the report and prepare
+#' them in a table for use in the bottom of the report
 #' 
-#' @param reportData Report data.
-#' @param table A vector from which to derive qualifiers from.
-#' @importFrom dplyr mutate
-#' @return A vector of strings.
+#' @param reportData The JSON data for the report requested
+#' 
+#' @param table A vector from which to derive qualifiers from that
+#' were displayed in the report itself.
+#'  
+#' @return A table to print at the bottom of the report or an empty
+#' data.frame if there are no qualifiers
+#' 
 srsQualifiersTable <- function(reportData, table) {
   #Construct List of all qualifiers
-  if(!isEmptyOrBlank(reportData$readings$qualifiers)){
+  if(!isEmptyOrBlank(reportData[["readings"]]$qualifiers)){
     qualifiersList <- data.frame(unlist(reportData[["readings"]][["qualifiers"]], recursive=FALSE))
   } else {
     qualifiersList <- data.frame()
@@ -323,22 +381,28 @@ srsQualifiersTable <- function(reportData, table) {
   
   #Construct a list of qualifiers used in the report
   usedQualifiers <- getSrsTableQualifiers(table)
-  qualifiersList <- qualifiersList[which(qualifiersList$code %in% usedQualifiers),]
+  qualifiersList <- qualifiersList[which(qualifiersList[["code"]] %in% usedQualifiers),]
   
-  toRet <- data.frame(stringsAsFactors = FALSE, qualifiersList$code, qualifiersList$identifier, qualifiersList$displayName)
+  toRet <- data.frame(stringsAsFactors = FALSE, qualifiersList[["code"]], qualifiersList[["identifier"]], qualifiersList[["displayName"]])
   toRet <- toRet[!duplicated(toRet), ]
   colnames(toRet) <- columnNames
 
   return(toRet)
 }
 
-
-
+#' Takes a list of data and extracts the qualifiers from it
+#' 
+#' @description when passed a list of data, extracts the qualifiers from it
+#' 
+#' @param table The table of data created for the SRS report
+#' 
+#' @return Extracts the unique qualifiers and returns a list
+#'
 getSrsTableQualifiers <- function(table){
   toRet <- list()
 
   #Extract Necessary Data Columns
-  relevantData <- strsplit(unlist(table$toRet$Qualifier[nchar(table$toRet$Qualifier) > 0]), ",")
+  relevantData <- strsplit(unlist(table[["toRet"]]$Qualifier[nchar(table[["toRet"]]$Qualifier) > 0]), ",")
 
   #Convert HTML codes back to equivalent characters
   relevantData <- lapply(relevantData, function(x){return(convertTableDisplayToString(x))})
