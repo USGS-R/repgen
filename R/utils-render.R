06ec7b59795f15136aea57474f14ae4a0ddbe381
Megan Hines <mhines@usgs.gov>
2017-01-19 15:49:17 -0600

    moving two formerly utils-render functions which feel out of place in the utils-html to new utils-render
    moving/adding docu

---
+++ R/utils-render.R
@@ -0,0 +1,64 @@
+#' Clean up temporary disk space used when rendering reports
+#' 
+#' @description deletes report components after they are used in render function
+#' and in case any old files are lingering (render crashed or errored for some reason) 
+#' also removes anything older than 5 minutes when the function is called
+#' 
+cleanTempSpace <- function() {
+  tempdir <- dirname(tempfile())
+  allTempFiles <- paste0(tempdir, "/", list.files(tempdir))
+  lastAccessTimes <- file.info(allTempFiles)$atime
+  
+  for (i in 1:length(allTempFiles)) { 
+    if(difftime(Sys.time(), lastAccessTimes[i], units="mins") > 5) { #delete anything older than 5 minutes
+      unlink(allTempFiles[i], recursive=TRUE)
+    }
+  }
+}
+
+#' Starting point, creates RMD and runs rendering
+#' 
+#' @description this function orchestrates the creation of the html reports
+#' 
+#' @param data coming in to create a plot
+#' @param author name of person generating the report
+#' @param reportName name of report being generated
+#' 
+#' @return the html file and other report bits
+#' 
+startRender <- function(data, author, reportName){
+  data <- data 
+  
+  wd <- getwd()
+  tmp_folder_name <- paste0('tmp-', reportName)
+  
+  output_dir <- paste0(wd, "/", tmp_folder_name)
+  
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir)
+  }
+  
+  #copy all shared files to tmp folder
+  shared_files <- list.files(system.file('shared', package = 'repgen'), full.names = TRUE)
+  file.copy(shared_files, output_dir)
+  
+  #copy all report inst files to tmp folder
+  report_files <- list.files(system.file(reportName, package = 'repgen'), full.names = TRUE)
+  file.copy(report_files, output_dir)
+  
+  if(reportName == "vdiagram"){
+    rmd_file <- makeVDiagramRmd(output_dir, data, output_dir)
+  } else {
+    rmd_file <- system.file(reportName, paste0(reportName, '.Rmd'), package = 'repgen')
+  }
+  
+  out_file <- render(rmd_file, paste0("html_document"), params = list(author=author), 
+                     output_dir = output_dir, intermediates_dir = output_dir, 
+                     output_options=c(paste0("lib_dir=", output_dir)), 
+                     clean=TRUE)
+  
+  #try to clean up old temp files
+  cleanTempSpace()
+  
+  return(out_file)
+}
