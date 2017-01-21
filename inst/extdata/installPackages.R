cdc220bbe01752c42393e05996c8274902273edc
Andrew Halper <ashalper@usgs.gov>
2017-01-20 17:29:13 -0700

    Steering towards configuration found on nwissddvasaqcu.

--- inst/extdata/installPackages.R
+++ inst/extdata/installPackages.R
@@ -1,29 +1,31 @@
-# the list of repgen, production prerequisite packages
+# Install repgen, production instance, prerequisite packages. Should be safe to
+# run repeatedly without additional intervention.
 
-# devtools is currently only required on a production instance to install 
-# gsplot from GitHub (see below). There are plans to "promote" it to CRAN (or 
-# GRAN?), but that hasn't happened yet. See
-# https://usgs-cida.slack.com/archives/aqcu/p1484934743005621 if it still
+# devtools is currently only required on a production instance to install gsplot
+# from GitHub (see below). There are plans to "promote" it to CRAN (or GRAN?),
+# but that hasn't happened yet. See 
+# https://usgs-cida.slack.com/archives/aqcu/p1484934743005621 if it still 
 # exists.
 pkgs <- c(
-  "dplyr", "devtools",
-  "htmlTable", "httr",
+  "devtools",
+  "dplyr",
+  "htmlTable",
   "jsonlite",
   "knitr",
   "lubridate",
   "rmarkdown"
 )
 
-installPackages <- function(pkgs) {
+installPackages <- function(pkgs, repos) {
   for (p in pkgs) {
     tryCatch({
       packageDescription(p) # check to see if package p is already installed
     },
     warning = function(w) {
       # if package p is not installed...
-      if (any(grepl("no package .+ was found", w))) {
+      if (any(grepl("(DESCRIPTION file of package .+ is missing or broken|no package .+ was found)", w))) {
         # ...install it
-        install.packages(p)
+        install.packages(p, repos = repos)
       } else {
         print(w)
       }
@@ -34,7 +36,10 @@ installPackages <- function(pkgs) {
   }
 }
 
-installPackages(pkgs)
+# all packages are held back to older versions, which are keyed by their repo
+# URLs below
+installPackages(pkgs, "http://mran.microsoft.com/snapshot/2016-03-31")
+installPackages(c("httr"), "http://mran.microsoft.com/snapshot/2016-01-27")
 
 # To be able to install gsplot from GitHub using devtools (below), download 
 # DOI root certificate and append to openssl package's certificate bundle
