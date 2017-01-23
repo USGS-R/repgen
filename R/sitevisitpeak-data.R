1d71a758cc2ff99b880b79e79370e276a82fbd93
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2017-01-23 15:49:15 -0600

    cleanup unused functions

--- R/sitevisitpeak-data.R
+++ R/sitevisitpeak-data.R
@@ -18,7 +18,7 @@ sitevisitpeakTable <- function(readingsData, excludeComments){
     
     quals <- formatQualifiersStringList(listElements$qualifiers[[1]])
     
-    diff <- getIvDifference(listElements$value, listElements$associatedIvValue)
+    diff <- readIvDifference(listElements$value, listElements$associatedIvValue)
     
     if(includeComments){
       toAdd = c(fvTimeFormatting$date,
@@ -60,102 +60,4 @@ sitevisitpeakTable <- function(readingsData, excludeComments){
   rownames(toRet) <- NULL
   
   return(toRet)
-}
-
-getQualifiers <- function(time, inQualifiers) {
-  if(length(inQualifiers) < 1) return("");
-  q <- inQualifiers[[1]]
-  
-  if(is.null(q) || length(q) < 1) return("");
-  
-  qualifiers <- q[time>q$startDate & q$endDate>time,]
-  
-  builtQualifiers <- ""
-  if(nrow(qualifiers) > 0) {
-    for(i in 1:nrow(qualifiers)) {
-      #Due to HTML hack being used for comments on SRS reports can't use kable to render table and thus need to use a hack to show greaterthan and other special HTML codes
-      #Same method is used here for consistency since both reports use HTML tables formatted in the same way
-      builtQualifiers <- paste0(builtQualifiers, convertStringToTableDisplay(qualifiers[i,]$code), ",")
-    }
-    strLength <- nchar(builtQualifiers)
-    if(strLength > 0) {
-      builtQualifiers <- substr(builtQualifiers, 1, strLength-1)
-    }
-  }
-  
-  return(builtQualifiers)
-}
-
-getIvDifference <- function(readingVal, ivVal) {
-  result <- "NA"
-  v1 <- as.numeric(readingVal)
-  v2 <- as.numeric(ivVal)
-  if(is.numeric(v1) & is.numeric(v2)) {
-    val <- v2-v1
-    if(!is.na(val) && all(c(length(v1),length(v2)) != 0)) {
-      result <- as.character(round(val, digits = nchar(ivVal)))
-      
-      if(abs(val) > 0.05) {
-        result <- paste(result, "**")
-      }
-    }
-  }
-  return(result)
-}
-
-containsOutsideUncertainty2 <- function(data) {
-  readings_diff <- list()
-  readings_data <- data$readings
-  for(listRows in row.names(readings_data)){
-    listElements <- readings_data[listRows,]
-    readings_diff <- append(readings_diff, getIvDifference(listElements$value, listElements$associatedIvValue))
-  }
-
-  return(length(readings_diff[grepl("\\*\\*", readings_diff)]) > 0)
-}
-
-#' Create Flat Text, "qualifiers table" Type Output Table
-#' 
-#' @param data Report data.
-#' @param table A vector from which to derive qualifiers from.
-#' @importFrom dplyr mutate
-#' @return A vector of strings.
-#' @export
-svpQualifiersTable <- function(data, table) {
-  #Construct List of all qualifiers
-  if(!isEmptyOrBlank(data$readings$associatedIvQualifiers)){
-      qualifiersList <- do.call("rbind", data$readings$associatedIvQualifiers)
-  } else {
-      qualifiersList <- data.frame()
-  }
-  
-  if (isEmptyOrBlank(qualifiersList) || nrow(qualifiersList) == 0) return ()
-  
-  columnNames <- c("Code",
-                   "Identifier",
-                   "Description")
-  
-  #Construct a list of qualifiers used in the report
-  usedQualifiers <- getSvpTableQualifiers(table)
-  qualifiersList <- qualifiersList[which(qualifiersList$code %in% usedQualifiers),]
-  
-  toRet <- data.frame(stringsAsFactors = FALSE, qualifiersList$code, qualifiersList$identifier, qualifiersList$displayName)
-  toRet <- toRet[!duplicated(toRet), ]
-  colnames(toRet) <- columnNames
-
-  return(toRet)
-}
-
-getSvpTableQualifiers <- function(table){
-  toRet <- list()
-
-  #Extract Necessary Data Columns
-  relevantData <- strsplit(unlist(table$Qualifier[nchar(table$Qualifier) > 0]), ",")
-
-  #Convert HTML codes back to equivalent characters
-  relevantData <- lapply(relevantData, function(x){return(convertTableDisplayToString(x))})
-    
-  toRet <- unlist(relevantData)
-
-  return(toRet[!duplicated(toRet)])
 }
