d348333ff1c8188ea93b4ccccb8d818f85b368ff
Andrew Halper <ashalper@usgs.gov>
2017-01-25 10:08:34 -0700

    Obsolete.

--- inst/extdata/dev-packages.R
+++
@@ -1,6 +0,0 @@
-# Install repgen development packages. These should only be necessary if you 
-# intend to do repgen development. They are not needed on a production server.
-
-source(paste0(getwd(), "/inst/extdata/installPackages.R"))
-
-installPackages(c("devtools", "roxygen2", "testthat"))
