1d94dbb5726ab4c1aaacf12c3d84649e32a0f71f
Andrew Halper <ashalper@usgs.gov>
2017-02-06 14:46:25 -0700

    Added code to derive list of packages to install from DESCRIPTION file.

--- inst/extdata/installPackages.R
+++ inst/extdata/installPackages.R
@@ -1,17 +1,91 @@
 # Install repgen, production instance, prerequisite packages. Should be safe to
 # run repeatedly without additional intervention.
 
-pkgs <- c(
-  "dplyr",
-  "evaluate",
-  "htmlTable",
-  "jsonlite",
-  "lazyeval",
-  "knitr",
-  "lubridate",
-  "rmarkdown",
-  "yaml"
-)
+# a slight modification of utils::packageDescription(), to read "Imports"
+# section of DESCRIPTION file
+repgenImports <- function (lib.loc = NULL, encoding = "") {
+  pkg <- "repgen"
+  fields <- "Imports"
+  retval <- list()
+  
+  if (!is.null(fields)) {
+    fields <- as.character(fields)
+    retval[fields] <- NA
+  }
+  
+  pkgpath <- if (is.null(lib.loc)) {
+    if (pkg == "base") 
+      file.path(.Library, "base")
+    else if (isNamespaceLoaded(pkg)) 
+      getNamespaceInfo(pkg, "path")
+    else if ((envname <- paste0("package:", pkg)) %in% search()) {
+      attr(as.environment(envname), "path")
+    }
+  }
+  
+  if (is.null(pkgpath)) 
+    pkgpath <- ""
+  
+  if (pkgpath == "") {
+    libs <- if (is.null(lib.loc))
+      .libPaths()
+    else
+      lib.loc
+    for (lib in libs)
+      if (file.access(file.path(lib, pkg), 5) == 0L) {
+        pkgpath <- file.path(lib, pkg)
+        break
+      }
+  }
+  
+  if (pkgpath == "") {
+    warning(gettextf("no package '%s' was found", pkg), 
+            domain = NA)
+    return(NA)
+  }
+  
+  if (file.exists(file <- file.path(pkgpath, "DESCRIPTION"))) {
+    dcf <- read.dcf(file = file)
+    if (NROW(dcf) < 1L) 
+      stop(gettextf("DESCRIPTION file of package '%s' is corrupt", 
+                    pkg), domain = NA)
+    desc <- as.list(dcf[1, ])
+  }
+  else file <- ""
+  
+  if (nzchar(file)) {
+    enc <- desc[["Encoding"]]
+    if (!is.null(enc) && !is.na(encoding)) {
+      if (missing(encoding) && Sys.getlocale("LC_CTYPE") == 
+          "C") 
+        encoding <- "ASCII//TRANSLIT"
+      newdesc <- try(lapply(desc, iconv, from = enc, to = encoding))
+      if (!inherits(newdesc, "try-error")) 
+        desc <- newdesc
+      else warning("'DESCRIPTION' file has an 'Encoding' field and re-encoding is not possible", 
+                   call. = FALSE)
+    }
+    if (!is.null(fields)) {
+      ok <- names(desc) %in% fields
+      retval[names(desc)[ok]] <- desc[ok]
+    }
+    else retval[names(desc)] <- desc
+  }
+  
+  if ((file == "") || (length(retval) == 0)) {
+    warning(gettextf("DESCRIPTION file of package '%s' is missing or broken", 
+                     pkg), domain = NA)
+    return(NA)
+  }
+  
+  class(retval) <- "repgenImports"
+  
+  if (!is.null(fields)) 
+    attr(retval, "fields") <- fields
+  
+  attr(retval, "file") <- file
+  retval
+}
 
 # convenience wrapper function around install.packages()
 installPackages <- function(pkgs, lib, repos = getOption("repos")) {
@@ -34,10 +108,18 @@ installPackages <- function(pkgs, lib, repos = getOption("repos")) {
   }
 }
 
-lib <- Sys.getenv("R_LIBS")
-if (nchar(lib) == 0) {
-  stop("Could not get a value for R_LIBS environment variable")
+nodename <- Sys.info()["nodename"]
+if (nodename == "IGSWZTWWWSASHA") {
+  lib <- .libPaths()[1]
+} else {
+  lib <- Sys.getenv("R_LIBS")
+  
+  if (nchar(lib) == 0) {
+    stop("Could not get a value for R_LIBS environment variable")
+  }
 }
+
+pkgs <- repgenImports(lib.loc = lib)
 
 # all packages except devtools and its prerequisites are held back to older
 # versions
