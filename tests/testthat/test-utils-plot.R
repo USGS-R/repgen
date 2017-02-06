1f73071ff343381cd9b9ed8dd4a7a1a137ab379a
zmoore-usgs <zmoore@usgs.gov>
2017-02-06 15:20:09 -0600

    AQCU-991: Added first set of tests for utils-plot.

--- tests/testthat/test-utils-plot.R
+++ tests/testthat/test-utils-plot.R
@@ -3,7 +3,233 @@ context("utils-plot tests")
 wd <- getwd()
 setwd(dir = tempdir())
 
-test_that('', {
+test_that('formatMinMaxLabel properly formats the min and max labels to be placed above the plot', {
+    ml <- list(
+        time = repgen:::flexibleTimeParse("2013-11-12T22:45:00-05:00", "Etc/GMT+5"),
+        value = 1,
+        legend.name = "Max. Instantaneous Discharge : 1"
+    )
+    units <- "ft^3/s"
+
+    label <- repgen:::formatMinMaxLabel(ml, units)
+
+    expect_is(label, 'character')
+    expect_equal(label, 'Max. Instantaneous Discharge : 1ft^3/s Nov 12, 2013 22:45:00 (UTC -05:00)')
+})
+
+test_that('XAxisLabelStyle properly creates X Axis labels', {
+    timezone <- "Etc/GMT+5"
+    startDate <- repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone)
+    endDate1 <- repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone)
+    endDate2 <- repgen:::flexibleTimeParse("2014-11-12T23:59:00-05:00", timezone)
+    plotDates1 <- c(
+         as.Date("2013-11-10T00:00:00-05:00"),
+         as.Date("2013-11-17T00:00:00-05:00"),
+         as.Date("2013-11-24T00:00:00-05:00"),
+         as.Date("2013-12-01T00:00:00-05:00")
+    )
+    plotDates2 <- c(
+        as.Date("2013-11-10 GMT+5"), as.Date("2013-11-17 GMT+5"), as.Date("2013-11-24 GMT+5"),
+        as.Date("2013-12-01 GMT+5"), as.Date("2013-12-08 GMT+5"), as.Date("2013-12-15 GMT+5"),
+        as.Date("2013-12-22 GMT+5"), as.Date("2013-12-29 GMT+5"), as.Date("2014-01-05 GMT+5"),
+        as.Date("2014-01-12 GMT+5"), as.Date("2014-01-19 GMT+5"), as.Date("2014-01-26 GMT+5"),
+        as.Date("2014-02-02 GMT+5"), as.Date("2014-02-09 GMT+5"), as.Date("2014-02-16 GMT+5"),
+        as.Date("2014-02-23 GMT+5"), as.Date("2014-03-02 GMT+5"), as.Date("2014-03-09 GMT+5"),
+        as.Date("2014-03-16 GMT+5"), as.Date("2014-03-23 GMT+5"), as.Date("2014-03-30 GMT+5"),
+        as.Date("2014-04-06 GMT+5"), as.Date("2014-04-13 GMT+5"), as.Date("2014-04-20 GMT+5"),
+        as.Date("2014-04-27 GMT+5"), as.Date("2014-05-04 GMT+5"), as.Date("2014-05-11 GMT+5"),
+        as.Date("2014-05-18 GMT+5"), as.Date("2014-05-25 GMT+5"), as.Date("2014-06-01 GMT+5"),
+        as.Date("2014-06-08 GMT+5"), as.Date("2014-06-15 GMT+5"), as.Date("2014-06-22 GMT+5"),
+        as.Date("2014-06-29 GMT+5"), as.Date("2014-07-06 GMT+5"), as.Date("2014-07-13 GMT+5"),
+        as.Date("2014-07-20 GMT+5"), as.Date("2014-07-27 GMT+5"), as.Date("2014-08-03 GMT+5"),
+        as.Date("2014-08-10 GMT+5"), as.Date("2014-08-17 GMT+5"), as.Date("2014-08-24 GMT+5"),
+        as.Date("2014-08-31 GMT+5"), as.Date("2014-09-07 GMT+5"), as.Date("2014-09-14 GMT+5"),
+        as.Date("2014-09-21 GMT+5"), as.Date("2014-09-28 GMT+5"), as.Date("2014-10-05 GMT+5"),
+        as.Date("2014-10-12 GMT+5"), as.Date("2014-10-19 GMT+5"), as.Date("2014-10-26 GMT+5"),
+        as.Date("2014-11-02 GMT+5"), as.Date("2014-11-09 GMT+5")
+    )
+    plot_object1 <- gsplot(ylog = FALSE, yaxs = 'i') %>%
+      grid(nx = 0, ny = NULL, equilogs = FALSE, lty = 3, col = "gray") %>%
+      axis(1, at = plotDates, labels = format(plotDates, "%b\n%d"), padj = 0.5) %>%
+      axis(2, reverse = FALSE, las=0) %>%
+      view(xlim = c(startDate, endDate1))
+
+    plot_object2 <- gsplot(ylog = FALSE, yaxs = 'i') %>%
+      grid(nx = 0, ny = NULL, equilogs = FALSE, lty = 3, col = "gray") %>%
+      axis(1, at = plotDates, labels = format(plotDates, "%b\n%d"), padj = 0.5) %>%
+      axis(2, reverse = FALSE, las=0) %>%
+      view(xlim = c(startDate, endDate2))
+
+    newPlot1 <- repgen:::XAxisLabelStyle(plot_object1, startDate, endDate1, timezone, plotDates1)
+    newPlot2 <- repgen:::XAxisLabelStyle(plot_object2, startDate, endDate2, timezone, plotDates2)
+
+    mtext1 <- gsplot:::views(newPlot1)[[1]][which(grepl("mtext", names(gsplot:::views(newPlot1)[[1]])))]
+    mtext2 <- gsplot:::views(newPlot2)[[1]][which(grepl("mtext", names(gsplot:::views(newPlot2)[[1]])))]
+
+    expect_equal(length(mtext1), 0)
+    expect_equal(length(mtext2), 2)
+})
+
+test_that('Calculate Limits properly calculates the limits of the provided data', {
+    timezone <- "Etc/GMT+5"
+
+    pts1 <- data.frame(
+        time = c(repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone), 
+                 repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone)),
+        value = c(10, 20)
+    )
+
+    pts2 <- data.frame(
+        time = c(repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone), 
+                 repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone),
+                 repgen:::flexibleTimeParse("2014-11-12T23:59:00-05:00", timezone)),
+        value = c(10, 20, 11)
+    )
+
+    lims1 <- repgen:::calculateLims(pts1)
+    lims2 <- repgen:::calculateLims(pts2)
+
+    expect_is(lims1, 'list')
+    expect_is(lims2, 'list')
+    expect_equal(lims1$xlim[[1]], pts1[['time']][[1]])
+    expect_equal(lims1$xlim[[2]], pts1[['time']][[2]])
+    expect_equal(lims1$ylim[[1]], 10)
+    expect_equal(lims1$ylim[[2]], 20)    
+    
+    expect_equal(lims2$xlim[[1]], pts2[['time']][[1]])
+    expect_equal(lims2$xlim[[2]], pts2[['time']][[3]])
+    expect_equal(lims2$ylim[[1]], 10)
+    expect_equal(lims2$ylim[[2]], 20)
+})
+
+test_that('plotItem properly adds an item to a GS Plot object with the proper styles', {
+    timezone <- "Etc/GMT+5"
+    startDate <- repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone)
+    endDate <- repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone)
+
+    plotDates <- c(
+         as.Date("2013-11-10T00:00:00-05:00"),
+         as.Date("2013-11-17T00:00:00-05:00"),
+         as.Date("2013-11-24T00:00:00-05:00"),
+         as.Date("2013-12-01T00:00:00-05:00")
+    )
+
+    pts <- data.frame(
+        time = c(repgen:::flexibleTimeParse("2013-11-11T12:00:00-05:00", timezone), 
+                 repgen:::flexibleTimeParse("2013-11-12T23:59:00-05:00", timezone)),
+        value = c(10, 20)
+    )
+
+    plot_object <- gsplot(ylog = FALSE, yaxs = 'i') %>%
+      grid(nx = 0, ny = NULL, equilogs = FALSE, lty = 3, col = "gray") %>%
+      axis(1, at = plotDates, labels = format(plotDates, "%b\n%d"), padj = 0.5) %>%
+      axis(2, reverse = FALSE, las=0) %>%
+      view(xlim = c(startDate, endDate1))
+
+    plot_object <- repgen:::plotItem(plot_object, pts, repgen:::getDVHydrographPlotConfig, list(pts, 'groundWaterLevels'), isDV=TRUE)
+  
+    expect_equal(length(plot_object$view.1.2$points$x), 2)
+    expect_equal(length(plot_object$view.1.2$points$y), 2)
+})
+
+test_that('plotTimeSeries properly adds a time series to a GS Plot object with the proper styles', {
+    timezone <- "Etc/GMT+5"
+    startDate <- repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone)
+    endDate <- repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone)
+
+    plotDates <- c(
+         as.Date("2013-11-10T00:00:00-05:00"),
+         as.Date("2013-11-17T00:00:00-05:00"),
+         as.Date("2013-11-24T00:00:00-05:00"),
+         as.Date("2013-12-01T00:00:00-05:00")
+    )
+
+    pts <- data.frame(
+        time = c(repgen:::flexibleTimeParse("2013-11-11T12:00:00-05:00", timezone), 
+                 repgen:::flexibleTimeParse("2013-11-12T23:59:00-05:00", timezone)),
+        value = c(10, 20)
+    )
+
+    timeSeries <- list(
+        points = pts
+    )
+
+    plot_object <- gsplot(ylog = FALSE, yaxs = 'i') %>%
+      grid(nx = 0, ny = NULL, equilogs = FALSE, lty = 3, col = "gray") %>%
+      axis(1, at = plotDates, labels = format(plotDates, "%b\n%d"), padj = 0.5) %>%
+      axis(2, reverse = FALSE, las=0) %>%
+      view(xlim = c(startDate, endDate1))
+
+    plot_object <- repgen:::plotTimeSeries(plot_object, timeSeries, 'stat1TimeSeries', timezone, repgen:::getDVHydrographPlotConfig, list(ylabel=""), isDV=TRUE)
+  
+    expect_equal(length(plot_object$view.1.2$lines$x), 3)
+    expect_equal(length(plot_object$view.1.2$lines$y), 3)
+})
+
+test_that('formatSplitTimeSeriesForPlotting properly formats a split time series for plotting', {
+    timezone <- "Etc/GMT+5"
+    startDate <- repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone)
+    endDate <- repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone)
+    plotDates <- c(
+         as.Date("2013-11-10T00:00:00-05:00"),
+         as.Date("2013-11-17T00:00:00-05:00"),
+         as.Date("2013-11-24T00:00:00-05:00"),
+         as.Date("2013-12-01T00:00:00-05:00")
+    )
+    pts <- data.frame(
+        time = c(repgen:::flexibleTimeParse("2013-11-11T12:00:00-05:00", timezone), 
+                 repgen:::flexibleTimeParse("2013-11-12T23:59:00-05:00", timezone)),
+        value = c(10, 20)
+    )
+    timeSeries <- list(
+        points = pts
+    )
+    tsList <- list(
+        timeSeries = timeSeries
+    )
+
+    formatted <- repgen:::formatSplitTimeSeriesForPlotting(tsList)
+
+    expect_is(formatted, 'list')
+    expect_equal(length(formatted$timeSeries), 2)
+    expect_equal(length(formatted$timeSeries$value), 2)
+    expect_equal(length(formatted$timeSeries$time), 2)
+    expect_equal(formatted$timeSeries$value[[1]], 10)
+    expect_equal(formatted$timeSeries$value[[2]], 20)
+    expect_equal(formatted$timeSeries$time[[1]], pts[[1]][[1]])
+    expect_equal(formatted$timeSeries$time[[2]], pts[[1]][[2]])
+})
+
+test_that('formatTimeSeriesForPlotting properly formats a time series for plotting', {
+    timezone <- "Etc/GMT+5"
+    startDate <- repgen:::flexibleTimeParse("2013-11-10T12:00:00-05:00", timezone)
+    endDate <- repgen:::flexibleTimeParse("2013-12-02T23:59:00-05:00", timezone)
+    plotDates <- c(
+         as.Date("2013-11-10T00:00:00-05:00"),
+         as.Date("2013-11-17T00:00:00-05:00"),
+         as.Date("2013-11-24T00:00:00-05:00"),
+         as.Date("2013-12-01T00:00:00-05:00")
+    )
+    pts <- data.frame(
+        time = c(repgen:::flexibleTimeParse("2013-11-11T12:00:00-05:00", timezone), 
+                 repgen:::flexibleTimeParse("2013-11-12T23:59:00-05:00", timezone)),
+        value = c(10, 20)
+    )
+    timeSeries <- list(
+        points = pts
+    )
+
+    formatted <- repgen:::formatTimeSeriesForPlotting(timeSeries)
+
+    expect_is(formatted, 'data.frame')
+    expect_equal(nrow(formatted), 2)
+    expect_equal(formatted[[1]][[1]], pts[[1]][[1]])
+    expect_equal(formatted[[1]][[2]], pts[[1]][[2]])
+    expect_equal(formatted[[2]][[1]], 10)
+    expect_equal(formatted[[2]][[2]], 20)
+})
+
+test_that('extendYaxisLimits properly extends the Y Axis limits', {
 
 })
 
