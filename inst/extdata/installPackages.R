1f0dc8589e8a67bd2f054c92f7cd9b2a2a60806a
Megan Hines <mhines@usgs.gov>
2017-03-10 08:48:30 -0600

    upgrade_dependencies=FALSE did not work, JR suggests trying depends=FALSE

--- inst/extdata/installPackages.R
+++ inst/extdata/installPackages.R
@@ -49,7 +49,7 @@ installPackages <- function(pkgs, lib, repos = getOption("repos")) {
         "(DESCRIPTION file of package .+ is missing or broken|no package .+ was found)",
         w))) {
         # ...install it
-        install.packages(p, lib, repos = repos, upgrade_dependencies=FALSE)
+        install.packages(p, lib, repos = repos, depends=FALSE)
       } else {
         print(w)
       }
