5493e20a2fa79b5020916a38a41e6634e2e53b87
zmoore-usgs <zmoore@usgs.gov>
2017-04-19 11:54:03 -0500

    AQCU-1052: Fixed a typo

--- R/extremes-data.R
+++ R/extremes-data.R
@@ -213,7 +213,7 @@ createDataRows <-
           relatedSet <- x$relatedUpchain
         }
 
-        if(!is.null(relatedSet) && !isDV)
+        if(!is.null(relatedSet) && !isDv)
         {
           relatedValue <- relatedSet$value
           
@@ -221,7 +221,7 @@ createDataRows <-
             relatedValue <- mergeAndStretch(x$points, relatedSet)
             footnote <- TRUE
           }
-        } else if(!isDV) {
+        } else if(!isDv) {
           footnote <- TRUE
         }
         
