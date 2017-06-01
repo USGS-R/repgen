feb6b8f633c08ba5b884551abf47adc3d8946893
zmoore-usgs <zmoore@usgs.gov>
2017-06-01 12:33:07 -0500

    AQCU-1112: Updated documentation. Added fetch tests.

--- tests/testthat/test-utils-fetch.R
+++ tests/testthat/test-utils-fetch.R
@@ -595,3 +595,285 @@ test_that('fetchExcludedControlConditions properly retrieves the excluded contro
   expect_equal(conditions[1,][['value']], 'Clear')
   expect_equal(conditions[1,][['name']], "CLEAR")
 })
+
+test_that('fetchGaps properly retrieves the gaps', {
+  gapJson <- fromJSON('{
+     "gaps": [
+        {
+          "startTime": "2016-11-23T00:00:00-05:00",
+          "endTime": "2016-11-23T12:00:00-05:00"
+        },
+        {
+          "startTime": "2016-11-23T12:00:00-05:00",
+          "endTime": "2016-11-24T00:00:00-05:00"
+        }
+     ]
+  }')
+  
+  gaps <- repgen:::fetchGaps(gapJson)
+  
+  expect_is(gaps, 'data.frame')
+  expect_equal(nrow(gaps), 2)
+  expect_equal(gaps[1,][['startTime']], '2016-11-23T00:00:00-05:00')
+  expect_equal(gaps[1,][['endTime']], "2016-11-23T12:00:00-05:00")
+})
+
+test_that('fetchUpchainSeries properly retrieves the related upchain series', {
+  upchainJson <- fromJSON('{
+     "upchainTs": [
+        {
+          "identifier": "Gage height.ft@01047200",
+          "parameter": "Gage height",
+          "parameterIdentifier": "Gage height",
+          "nwisName": "Gage height",
+          "nwisPcode": "00065",
+          "unit": "ft",
+          "computation": "Instantaneous",
+          "timezone": "Etc/GMT+5",
+          "inverted": false,
+          "groundWater": false,
+          "discharge": false,
+          "sublocation": "",
+          "timeSeriesType": "ProcessorDerived",
+          "period": "Points",
+          "publish": true,
+          "primary": true,
+          "uniqueId": "5eb2fdadf2784ebeaed2f64c6d02edf8"
+        }
+     ]
+  }')
+  
+  upchain <- repgen:::fetchUpchainSeries(upchainJson)
+  
+  expect_is(upchain, 'data.frame')
+  expect_equal(nrow(upchain), 1)
+  expect_equal(upchain[1,][['identifier']], 'Gage height.ft@01047200')
+  expect_equal(upchain[1,][['uniqueId']], "5eb2fdadf2784ebeaed2f64c6d02edf8")
+})
+
+test_that('fetchDownchainSeries properly retrieves the related upchain series', {
+  downchainJson <- fromJSON('{
+     "downchainTs": [
+        {
+          "identifier": "Discharge.ft^3/s.diff_per@01047200",
+          "parameter": "Discharge",
+          "parameterIdentifier": "Discharge",
+          "nwisName": "Discharge",
+          "nwisPcode": "00060",
+          "unit": "ft^3/s",
+          "computation": "Instantaneous",
+          "timezone": "Etc/GMT+5",
+          "inverted": false,
+          "groundWater": false,
+          "discharge": true,
+          "sublocation": "",
+          "timeSeriesType": "ProcessorDerived",
+          "period": "Points",
+          "publish": false,
+          "primary": false,
+          "uniqueId": "884fc0c281b14685baf9cbf744f0a606"
+        }
+     ]
+  }')
+  
+  downchain <- repgen:::fetchDownchainSeries(downchainJson)
+  
+  expect_is(downchain, 'data.frame')
+  expect_equal(nrow(downchain), 1)
+  expect_equal(downchain[1,][['identifier']], 'Discharge.ft^3/s.diff_per@01047200')
+  expect_equal(downchain[1,][['uniqueId']], "884fc0c281b14685baf9cbf744f0a606")
+})
+
+test_that('fetchQualifiers properly retrieves the gaps', {
+  qualsJson <- fromJSON('{
+    "primaryTsMetadata": {
+      "qualifiers": [
+        {
+          "startDate": "2017-03-05T18:45:00-05:00",
+          "endDate": "2017-03-06T05:45:00.0000001-05:00",
+          "identifier": "EQUIP",
+          "code": "EQP",
+          "displayName": "Equipment malfunction",
+          "appliedBy": "system",
+          "dateApplied": "2017-03-11T14:57:13.4625975Z"
+        },
+        {
+          "startDate": "2017-02-26T01:30:00-05:00",
+          "endDate": "2017-02-26T01:30:00.0000001-05:00",
+          "identifier": "EQUIP",
+          "code": "EQP",
+          "displayName": "Equipment malfunction",
+          "appliedBy": "system",
+          "dateApplied": "2017-03-11T14:57:13.4625975Z"
+        }
+      ]
+    }
+  }')
+  
+  quals <- repgen:::fetchQualifiers(qualsJson)
+  
+  expect_is(quals, 'data.frame')
+  expect_equal(nrow(quals), 2)
+  expect_equal(quals[1,][['startDate']], '2017-03-05T18:45:00-05:00')
+  expect_equal(quals[1,][['endDate']], "2017-03-06T05:45:00.0000001-05:00")
+  expect_equal(quals[1,][['identifier']], "EQUIP")
+  expect_equal(quals[1,][['code']], "EQP")
+})
+
+test_that('fetchNotes properly retrieves the notes', {
+  notesJson <- fromJSON('{
+    "primaryTsMetadata": {
+      "notes": [
+        {
+          "startDate": "2017-02-24T12:30:00-05:00",
+          "endDate": "2017-02-24T14:00:00.0000001-05:00",
+          "note": "ADAPS Source Flag: *"
+        }
+      ]
+    }
+  }')
+  
+  notes <- repgen:::fetchNotes(notesJson)
+  
+  expect_is(notes, 'data.frame')
+  expect_equal(nrow(notes), 1)
+  expect_equal(notes[1,][['startDate']], '2017-02-24T12:30:00-05:00')
+  expect_equal(notes[1,][['endDate']], "2017-02-24T14:00:00.0000001-05:00")
+  expect_equal(notes[1,][['note']], "ADAPS Source Flag: *")
+})
+
+test_that('fetchGrades properly retrieves the grades', {
+  gradesJson <- fromJSON('{
+    "primaryTsMetadata": {
+      "grades": [
+        {
+          "startDate": "2016-05-01T00:00:00-05:00",
+          "endDate": "2017-05-31T00:00:00.0000001-05:00",
+          "code": "50"
+        }
+      ]
+    }
+  }')
+  
+  grades <- repgen:::fetchGrades(gradesJson)
+  
+  expect_is(grades, 'data.frame')
+  expect_equal(nrow(grades), 1)
+  expect_equal(grades[1,][['startDate']], '2016-05-01T00:00:00-05:00')
+  expect_equal(grades[1,][['endDate']], "2017-05-31T00:00:00.0000001-05:00")
+  expect_equal(grades[1,][['code']], "50")
+})
+
+test_that('fetchRatingCurves properly retrieves the rating cruves', {
+  curvesJson <- fromJSON('{
+    "ratingCurves": [
+      {
+        "curveNumber": "6.2",
+        "ratingShifts": [
+          {
+            "curveNumber": "6.2",
+            "shiftPoints": [
+              0.03,
+              0.12,
+              0
+            ],
+            "stagePoints": [
+              3.9,
+              5.3,
+              7.1
+            ],
+            "applicableStartDateTime": "2015-10-06T16:06:01-05:00",
+            "applicableEndDateTime": "9999-12-31T23:59:59.9999999Z",
+            "shiftNumber": 0,
+            "shiftRemarks": "Continued WY2015 BRS from rating 6.1. ARC"
+          }
+        ],
+        "baseRatingTable": {
+          "inputValues": [
+            3.6,
+            3.79,
+            3.9,
+            8.3,
+            9,
+            10,
+            11
+          ],
+          "outputValues": [
+            1.473,
+            3.629,
+            5.53,
+            1400,
+            2058,
+            3433,
+            5400
+          ]
+        },
+        "offsets": {
+          "inputValues": [
+            null
+          ],
+          "offSetValues": [
+            3.05
+          ]
+        },
+        "startOfPeriod": "2015-10-06T16:06:01-05:00",
+        "endOfPeriod": "2016-11-16T00:00:00-05:00",
+        "remarks": "Lowend extension for coverage in WY2016, base on measurements 104-107. Same as rating 6.1 above 3.90 ft. Was extended -.30 ft below 3.90 ft for low water coverage in WY2016-2017. ARC",
+        "ratingType": "LogarithmicTable",
+        "applicablePeriods": [
+          {
+            "startTime": "2015-10-06T16:06:01-05:00",
+            "endTime": "2016-11-16T00:00:00-05:00",
+            "remarks": "Started rating at beginning of new period worked. ARC"
+          },
+          {
+            "startTime": "2016-11-16T00:00:00-05:00",
+            "endTime": "9999-12-31T23:59:59.9999999Z",
+            "remarks": "Started rating at beginning of new period worked. ARC"
+          }
+        ]
+      }
+    ]
+  }')
+  
+  curves <- repgen:::fetchRatingCurves(curvesJson)
+  
+  expect_is(curves, 'data.frame')
+  expect_equal(nrow(curves), 1)
+  expect_equal(curves[1,][['startOfPeriod']], '2015-10-06T16:06:01-05:00')
+  expect_equal(curves[1,][['endOfPeriod']], "2016-11-16T00:00:00-05:00")
+  expect_equal(curves[1,][['curveNumber']], "6.2")
+  expect_equal(nrow(curves[1,][['applicablePeriods']][[1]]), 2)
+})
+
+test_that('fetchApprovals properly retrieves the approvals', {
+  approvalsJson <- fromJSON('{
+    "approvals": [
+      {
+        "level": 1200,
+        "description": "Approved",
+        "comment": "",
+        "dateApplied": "2017-02-02T21:16:24.937095Z",
+        "startTime": "2007-10-01T00:00:00-05:00",
+        "endTime": "2016-11-16T00:00:00-05:00"
+      },
+      {
+        "level": 900,
+        "description": "Working",
+        "comment": "",
+        "dateApplied": "2017-02-02T21:15:49.5368596Z",
+        "startTime": "2016-11-16T00:00:00-05:00",
+        "endTime": "9999-12-31T23:59:59.9999999Z"
+      }
+    ]
+  }')
+  
+  approvals <- repgen:::fetchApprovals(approvalsJson)
+  
+  expect_is(approvals, 'data.frame')
+  expect_equal(nrow(approvals), 2)
+  expect_equal(approvals[1,][['startTime']], '2007-10-01T00:00:00-05:00')
+  expect_equal(approvals[1,][['endTime']], "2016-11-16T00:00:00-05:00")
+  expect_equal(approvals[1,][['level']], 1200)
+  expect_equal(approvals[1,][['description']], "Approved")
+})
