89b65f6d26ebfe8f179575c35969917d6f55ec48
Megan Hines <mhines@usgs.gov>
2017-06-02 09:08:51 -0500

    AQCU-929: forgot to include something in this test

--- tests/testthat/test-uvhydrograph-render.R
+++ tests/testthat/test-uvhydrograph-render.R
@@ -296,7 +296,7 @@ test_that("createPrimaryPlot correctly configured gsplot",{
       comment=c("correction 1", "correction 2", "correction 3"), 
       stringsAsFactors=FALSE)
   
-  testRatings <- data.frame(curveNumber=c('7.0','7.0','7.0'), 
+  testRatingShifts <- data.frame(curveNumber=c('7.0','7.0','7.0'), 
       shiftPoints=c(0, 0.04, 0), 
       stagePoints=c(0, 4, 6), 
       applicableStartDateTime=c(as.POSIXct("2015-04-09 23:00:00"), as.POSIXct("2015-04-15 15:30:00"), as.POSIXct("2015-10-14 08:23:00")),
@@ -320,6 +320,7 @@ test_that("createPrimaryPlot correctly configured gsplot",{
       readings, 
       approvalBars,
       testCorrections,
+      testRatingShifts,
       TRUE,
       "Etc/GMT", 
       TRUE)
