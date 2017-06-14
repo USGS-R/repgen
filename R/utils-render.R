c90a3d599b7d7013694864f485ab8bc4eff3fab7
Megan Hines <mhines@usgs.gov>
2017-06-14 10:47:34 -0500

    AQCU-1125: adding no processing icon to legend; moving bits into derivation chain data/render where they belong.

--- R/utils-render.R
+++ R/utils-render.R
@@ -98,9 +98,6 @@ startTemplatedRender <- function(reportJson, author){
   #load header image
   templateData[["usgsLogoBase64"]] <- loadUsgsLogoBase64()
   
-  #load symbology processor images for derivation chain report
-  templateData[["derivationChainSymbols"]] <- loadLegendSymbologyDerivationChain()
-  
   mainReportTemplate <- loadCommonTemplate('report')
       
   #load header template
@@ -195,24 +192,6 @@ loadReportTemplate <- function(reportName, templateName) {
 #' @importFrom base64enc base64encode
 loadUsgsLogoBase64 <- function() {
   return(base64encode(system.file('shared','usgs_logo.jpg', package = 'repgen')))
-}
-
-#' load legend symbology for derivation chain report
-#' @description will load the legend icons needed and convert to base64 text
-#' @return base64 string representation of legend images
-#' @importFrom base64enc base64encode
-loadLegendSymbologyDerivationChain <- function() {
-  ratingModelDerived <- base64encode(system.file('templates/derivationchain','time-series-icons-rating-model-derived.png', package='repgen'))
-  passThrough <- base64encode(system.file('templates/derivationchain','time-series-icons-derived-pass-through.png', package='repgen'))
-  calculation <- base64encode(system.file('templates/derivationchain','time-series-icons-calculated.png', package='repgen'))
-  basic <- base64encode(system.file('templates/derivationchain','time-series-icons-basic.png', package='repgen'))
-  statDerived <- base64encode(system.file('templates/derivationchain','time-series-icons-statistical-derived.png', package='repgen'))
-  external <- base64encode(system.file('templates/derivationchain','time-series-icons-external.png', package='repgen'))
-  fillMissingData <- base64encode(system.file('templates/derivationchain','time-series-icons-fill-missing-data-derived.png', package='repgen'))
-  conditionalFill <- base64encode(system.file('templates/derivationchain','time-series-icons-fill-missing-data-derived.png', package='repgen'))
-  verticalDatumConversion <- base64encode(system.file('templates/derivationchain','time-series-icons-datum-conversion-derived.png', package='repgen'))
-  
-  return(list(ratingModelDerived=ratingModelDerived, passThrough=passThrough, calculation=calculation, basic=basic, statDerived=statDerived, external=external, fillMissingData=fillMissingData, conditionalFill=conditionalFill, verticalDatumConversion=verticalDatumConversion))
 }
 
 #' loadPartialsListForReport
