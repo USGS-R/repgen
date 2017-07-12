4e8f59445f0fb77c7c0b1271156a9ed59cfdf680
zmoore-usgs <zmoore@usgs.gov>
2017-07-12 17:03:48 -0500

    AQCU-1196: Fix for arrows collapse panel arrows not updating once the TSS report has been saved.

--- inst/templates/timeseriessummary/custom.js
+++ inst/templates/timeseriessummary/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     Sortable.initTable($('table')[i]);
     $($('table')[i]).attr("data-sortable-initialized", true);
   }
-})
+});
 
 $('#expand-all').on('click', function () {
     $('.accordion-toggle.collapsed.has-data').parent().siblings('.panel-collapse').collapse('show');
@@ -16,3 +16,11 @@ $('#expand-all').on('click', function () {
 $('#collapse-all').on('click', function () {
     $('.accordion-toggle.has-data').parent().siblings('.panel-collapse').collapse('hide');
 });
+
+$('.accordion-toggle').on('click', function (event){
+  if($(event.target).hasClass('collapsed')){
+    $(event.target).removeClass('collapsed')
+  } else {
+    $(event.target).addClass('collapsed')
+  }
+})
