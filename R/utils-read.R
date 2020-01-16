b10761facbac8b23ed1eef39ad9eaf6475a4ba67
Zack Moore <zmoore@usgs.gov>
2020-01-16 10:52:02 -0600

    Removed logic for moving IV information to highest reading. Moving this to Java.

--- R/utils-read.R
+++ R/utils-read.R
@@ -117,12 +117,6 @@ readFieldVisitReadings <- function(reportObject){
   associatedIvValue <- visitTime <- associatedIvTime <-associatedIvQualifiers <- value <- '.dplyr.var'
   
   if(validateFetchedData(visitReadings, "Readings", requiredFields, stopEmpty=TRUE)){
-    #Move associated IV information to the highest valued reading
-    if(!all(sapply(visitReadings$associatedIvValue, function(e){isEmptyOrBlank(e)}))){
-      orderedIVs <- visitReadings %>% dplyr::mutate(sort = is.na(associatedIvValue)) %>% dplyr::group_by(visitTime) %>% dplyr::arrange(visitTime, sort) %>% dplyr::ungroup() %>% dplyr::select(associatedIvValue, associatedIvTime, associatedIvQualifiers)
-      orderedValues <- visitReadings %>% dplyr::mutate(sort = as.numeric(value)) %>% dplyr::group_by(visitTime) %>% dplyr::arrange(visitTime, desc(sort)) %>% dplyr::select(-sort, -associatedIvValue, -associatedIvTime, -associatedIvQualifiers)
-      visitReadings <- dplyr::bind_cols(orderedValues, orderedIVs) %>% dplyr::arrange(visitTime) %>% as.data.frame()
-    }
     
     #Format the data frame to a table
     for(listRows in row.names(visitReadings)){
